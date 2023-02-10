fe0557db150c7740d23875749ec6e041d45c8383
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-02-10 19:56:06 -0400

    Removed boilerplate code

---
+++ src/components/Menu/BottomBar.svelte
@@ -0,0 +1,5 @@
+<script>
+  // import Button from './Menu/Button.svelte';
+</script>
+
+<nav class="flex rounded-xl border border-slate-300">hi</nav>
---
+++ src/components/Menu/Button.svelte
@@ -0,0 +1,10 @@
+<script>
+  /**
+   * @type {string}
+   */
+  export let url;
+</script>
+
+<button class="flex h-12 w-12 items-center justify-center rounded-lg">
+  <img class="h-8 w-8" src={`/images/ui/${url}.png`} alt="Menu UI Icon" />
+</button>
