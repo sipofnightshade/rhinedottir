0bc79e29d1fc31c67c443e5a54e8b980c31aed5e
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-02-11 11:08:13 -0400

    Removed mx-5 from BottomBar component

--- src/components/Menu/BottomBar.svelte
+++ src/components/Menu/BottomBar.svelte
@@ -2,7 +2,7 @@
 	import Button from './Button.svelte';
 </script>
 
-<nav class="mx-5 flex justify-between rounded-xl border border-slate-600 p-1">
+<nav class="flex justify-between rounded-xl border border-slate-600 p-1">
 	<Button url="UI_BtnIcon_Feedback.png" selected={false} />
 	<Button url="UI_BtnIcon_CharacterData.png" selected={false} />
 	<Button url="UI_BtnIcon_Team.png" selected={true} />
