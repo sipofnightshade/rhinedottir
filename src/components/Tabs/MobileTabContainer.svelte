5f1c1dab58871e4a21946044627c5b5a87ae81fd
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-02-13 19:17:07 -0400

    MobileTabContainer completed

---
+++ src/components/Tabs/MobileTabContainer.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Combo from '../Table/Combo.svelte';
+	import Damage from '../Table/Damage.svelte';
+	import Stats from '../Table/Stats.svelte';
+	import Tabs from './Tabs.svelte';
+
+	let items: any = [
+		{ label: 'Combos', value: 1, component: Combo },
+		{ label: 'Damage', value: 2, component: Damage },
+		{ label: 'Stats', value: 3, component: Stats }
+	];
+</script>
+
+<section class="h-3/4">
+	<Tabs {items} height={'h-mb-screen'} />
+</section>
