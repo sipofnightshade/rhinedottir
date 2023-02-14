fb83c81d8974556abd85ed0ad765b98b33a4e4b0
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-02-13 22:10:53 -0400

    Combo row "initial" design completed

---
+++ src/components/TableRow/ComboRow.svelte
@@ -0,0 +1,18 @@
+<script>
+  import ComboButton from './ComboButton.svelte';
+</script>
+
+<section class="my-1 border-b border-slate-700 pb-2">
+  <div class="flex">
+    <ComboButton />
+    <ComboButton />
+    <ComboButton />
+  </div>
+  <div class="flex justify-between">
+    <div class="mt-1 flex">
+      <img class="h-5 w-5 self-center" src="/images/elements/anemo.svg" alt="element" />
+      <span>246,192</span>
+    </div>
+    <button class="">Edit</button>
+  </div>
+</section>
