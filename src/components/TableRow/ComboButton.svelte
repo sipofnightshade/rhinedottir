56a99b46fbccb24ea0e0127060c058640e390a7e
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-02-13 21:07:57 -0400

    ComboButton design completed

---
+++ src/components/TableRow/ComboButton.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  // export let tag:string;
+  // export let img:string;
+  let reaction = true;
+</script>
+
+<button
+  class="mr-2 flex h-16 w-10 flex-col items-center justify-center rounded-lg bg-slate-700 "
+>
+  <span>N1</span>
+  <div class=" flex h-6 w-full justify-center">
+    <img class="h-5 w-5 self-center" src="/images/elements/anemo.svg" alt="element" />
+    {#if reaction === true}
+      <img
+        class="relative z-10 -ml-[14px] h-6 w-6 self-center"
+        src="/images/elements/separator.svg"
+        alt="element"
+      />
+      <img
+        class="-ml-[14px] h-5 w-5 self-center"
+        src="/images/elements/hydro.svg"
+        alt="element"
+      />
+    {/if}
+  </div>
+</button>
