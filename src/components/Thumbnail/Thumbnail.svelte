819f5deebbc9e858561328abeb9d56ab90ccd77e
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-02-19 09:48:22 -0400

    ✅Thumbnail component completed

---
+++ src/components/Thumbnail/Thumbnail.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let vision: string | undefined;
+  export let img: string;
+  export let alt: string;
+  //   export let rating: 5 | 4 | 3 | 1;
+</script>
+
+<div class="relative aspect-square overflow-hidden rounded-xl bg-slate-800">
+  <img src={img} alt="{alt} thumbnail" />
+  {#if vision}
+    <div
+      class="absolute top-1 left-1 w-1/4 rounded  outline-1 outline-offset-1 outline-slate-200"
+    >
+      <img src="/images/elements/{vision}.svg" alt="{vision} icon" />
+    </div>
+  {/if}
+</div>
