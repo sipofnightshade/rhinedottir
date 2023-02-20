3db16400396726b2ab420f5d495e0673e1fa0a41
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-02-20 10:49:18 -0400

    Added CharacterData with fullname

---
+++ src/lib/data/Characters.ts
@@ -0,0 +1,514 @@
+export const CharacterData = [
+  {
+    name: 'aether',
+    fullName: 'Aether',
+    rating: 5,
+    weapon: 'sword',
+    vision: 'anemo',
+    specialized: 'atk%'
+  },
+  {
+    name: 'aether',
+    fullName: 'Aether',
+    rating: 5,
+    weapon: 'sword',
+    vision: 'geo',
+    specialized: 'atk%'
+  },
+  {
+    name: 'aether',
+    fullName: 'Aether',
+    rating: 5,
+    weapon: 'sword',
+    vision: 'electro',
+    specialized: 'atk%'
+  },
+  {
+    name: 'aether',
+    fullName: 'Aether',
+    rating: 5,
+    weapon: 'sword',
+    vision: 'dendro',
+    specialized: 'atk%'
+  },
+  {
+    name: 'albedo',
+    fullName: 'Albedo',
+    rating: 5,
+    weapon: 'sword',
+    vision: 'geo',
+    specialized: 'elemental'
+  },
+  {
+    name: 'aloy',
+    fullName: 'Aloy',
+    rating: 5,
+    weapon: 'bow',
+    vision: 'cryo',
+    specialized: 'elemental'
+  },
+  {
+    name: 'amber',
+    fullName: 'Amber',
+    rating: 4,
+    weapon: 'bow',
+    vision: 'pyro',
+    specialized: 'atk%'
+  },
+  {
+    name: 'aratakiitto',
+    fullName: 'Arataki Itto',
+    rating: 5,
+    weapon: 'claymore',
+    vision: 'geo',
+    specialized: 'critrate'
+  },
+  {
+    name: 'barbara',
+    fullName: 'Barbara',
+    rating: 4,
+    weapon: 'catalyst',
+    vision: 'hydro',
+    specialized: 'hp%'
+  },
+  {
+    name: 'beidou',
+    fullName: 'Beidou',
+    rating: 4,
+    weapon: 'claymore',
+    vision: 'electro',
+    specialized: 'elemental'
+  },
+  {
+    name: 'bennett',
+    fullName: 'Bennett',
+    rating: 4,
+    weapon: 'sword',
+    vision: 'pyro',
+    specialized: 'energy'
+  },
+  {
+    name: 'candace',
+    fullName: 'Candace',
+    rating: 4,
+    weapon: 'polearm',
+    vision: 'hydro',
+    specialized: 'hp%'
+  },
+  {
+    name: 'chongyun',
+    fullName: 'Chongyun',
+    rating: 4,
+    weapon: 'claymore',
+    vision: 'cryo',
+    specialized: 'atk%'
+  },
+  {
+    name: 'collei',
+    fullName: 'Collei',
+    rating: 4,
+    weapon: 'bow',
+    vision: 'dendro',
+    specialized: 'atk%'
+  },
+  {
+    name: 'cyno',
+    fullName: 'Cyno',
+    rating: 5,
+    weapon: 'polearm',
+    vision: 'electro',
+    specialized: 'critdmg'
+  },
+  {
+    name: 'diluc',
+    fullName: 'Diluc',
+    rating: 5,
+    weapon: 'claymore',
+    vision: 'pyro',
+    specialized: 'critrate'
+  },
+  {
+    name: 'diona',
+    fullName: 'Diona',
+    rating: 4,
+    weapon: 'bow',
+    vision: 'cryo',
+    specialized: 'elemental'
+  },
+  {
+    name: 'dori',
+    fullName: 'Dori',
+    rating: 4,
+    weapon: 'claymore',
+    vision: 'electro',
+    specialized: 'hp%'
+  },
+  {
+    name: 'eula',
+    fullName: 'Eula',
+    rating: 5,
+    weapon: 'claymore',
+    vision: 'cryo',
+    specialized: 'critdmg'
+  },
+  {
+    name: 'faruzan',
+    fullName: 'Faruzan',
+    rating: 4,
+    weapon: 'bow',
+    vision: 'anemo',
+    specialized: 'atk%'
+  },
+  {
+    name: 'fischl',
+    fullName: 'Fischl',
+    rating: 4,
+    weapon: 'bow',
+    vision: 'electro',
+    specialized: 'atk%'
+  },
+  {
+    name: 'ganyu',
+    fullName: 'Ganyu',
+    rating: 5,
+    weapon: 'bow',
+    vision: 'cryo',
+    specialized: 'critdmg'
+  },
+  {
+    name: 'gorou',
+    fullName: 'Gorou',
+    rating: 4,
+    weapon: 'bow',
+    vision: 'geo',
+    specialized: 'elemental'
+  },
+  {
+    name: 'hutao',
+    fullName: 'Hu Tao',
+    rating: 5,
+    weapon: 'polearm',
+    vision: 'pyro',
+    specialized: 'critdmg'
+  },
+  {
+    name: 'jean',
+    fullName: 'Jean',
+    rating: 5,
+    weapon: 'sword',
+    vision: 'anemo',
+    specialized: 'healing'
+  },
+  {
+    name: 'kaedeharakazuha',
+    fullName: 'Kaedehara Kazuha',
+    rating: 5,
+    weapon: 'sword',
+    vision: 'anemo',
+    specialized: 'em'
+  },
+  {
+    name: 'kaeya',
+    fullName: 'Kaeya',
+    rating: 4,
+    weapon: 'sword',
+    vision: 'cryo',
+    specialized: 'energy'
+  },
+  {
+    name: 'kamisatoayaka',
+    fullName: 'Kamisato Ayaka',
+    rating: 5,
+    weapon: 'sword',
+    vision: 'cryo',
+    specialized: 'critdmg'
+  },
+  {
+    name: 'kamisatoayato',
+    fullName: 'Kamisato Ayato',
+    rating: 5,
+    weapon: 'sword',
+    vision: 'hydro',
+    specialized: 'critdmg'
+  },
+  {
+    name: 'keqing',
+    fullName: 'Keqing',
+    rating: 5,
+    weapon: 'sword',
+    vision: 'electro',
+    specialized: 'critdmg'
+  },
+  {
+    name: 'klee',
+    fullName: 'Klee',
+    rating: 5,
+    weapon: 'catalyst',
+    vision: 'pyro',
+    specialized: 'elemental'
+  },
+  {
+    name: 'kujousara',
+    fullName: 'Kujou Sara',
+    rating: 4,
+    weapon: 'bow',
+    vision: 'electro',
+    specialized: 'atk%'
+  },
+  {
+    name: 'kukishinobu',
+    fullName: 'Kuki Shinobu',
+    rating: 4,
+    weapon: 'sword',
+    vision: 'electro',
+    specialized: 'hp%'
+  },
+  {
+    name: 'layla',
+    fullName: 'Layla',
+    rating: 4,
+    weapon: 'sword',
+    vision: 'cryo',
+    specialized: 'hp%'
+  },
+  {
+    name: 'lisa',
+    fullName: 'Lisa',
+    rating: 4,
+    weapon: 'catalyst',
+    vision: 'electro',
+    specialized: 'em'
+  },
+  {
+    name: 'mona',
+    fullName: 'Mona',
+    rating: 5,
+    weapon: 'catalyst',
+    vision: 'hydro',
+    specialized: 'energy'
+  },
+  {
+    name: 'nahida',
+    fullName: 'Nahida',
+    rating: 5,
+    weapon: 'catalyst',
+    vision: 'dendro',
+    specialized: 'em'
+  },
+  {
+    name: 'nilou',
+    fullName: 'Nilou',
+    rating: 5,
+    weapon: 'sword',
+    vision: 'hydro',
+    specialized: 'hp%'
+  },
+  {
+    name: 'ningguang',
+    fullName: 'Ningguang',
+    rating: 4,
+    weapon: 'catalyst',
+    vision: 'geo',
+    specialized: 'elemental'
+  },
+  {
+    name: 'noelle',
+    fullName: 'Noelle',
+    rating: 4,
+    weapon: 'claymore',
+    vision: 'geo',
+    specialized: 'def%'
+  },
+  {
+    name: 'qiqi',
+    fullName: 'Qiqi',
+    rating: 5,
+    weapon: 'sword',
+    vision: 'cryo',
+    specialized: 'healing'
+  },
+  {
+    name: 'raidenshogun',
+    fullName: 'Raiden Shogun',
+    rating: 5,
+    weapon: 'polearm',
+    vision: 'electro',
+    specialized: 'energy'
+  },
+  {
+    name: 'razor',
+    fullName: 'Razor',
+    rating: 4,
+    weapon: 'claymore',
+    vision: 'electro',
+    specialized: 'physical'
+  },
+  {
+    name: 'rosaria',
+    fullName: 'Rosaria',
+    rating: 4,
+    weapon: 'polearm',
+    vision: 'cryo',
+    specialized: 'atk%'
+  },
+  {
+    name: 'sangonomiyakokomi',
+    fullName: 'Sangonomiya Kokomi',
+    rating: 5,
+    weapon: 'catalyst',
+    vision: 'hydro',
+    specialized: 'elemental'
+  },
+  {
+    name: 'sayu',
+    fullName: 'Sayu',
+    rating: 4,
+    weapon: 'claymore',
+    vision: 'anemo',
+    specialized: 'em'
+  },
+  {
+    name: 'shenhe',
+    fullName: 'Shenhe',
+    rating: 5,
+    weapon: 'polearm',
+    vision: 'cryo',
+    specialized: 'atk%'
+  },
+  {
+    name: 'shikanoinheizou',
+    fullName: 'Shikanoin Heizou',
+    rating: 4,
+    weapon: 'catalyst',
+    vision: 'anemo',
+    specialized: 'atk%'
+  },
+  {
+    name: 'sucrose',
+    fullName: 'Sucrose',
+    rating: 5,
+    weapon: 'catalyst',
+    vision: 'anemo',
+    specialized: 'atk%'
+  },
+  {
+    name: 'tartaglia',
+    fullName: 'Tartaglia',
+    rating: 5,
+    weapon: 'bow',
+    vision: 'hydro',
+    specialized: 'elemental'
+  },
+  {
+    name: 'thoma',
+    fullName: 'Thoma',
+    rating: 4,
+    weapon: 'polearm',
+    vision: 'pyro',
+    specialized: 'atk%'
+  },
+  {
+    name: 'tighnari',
+    fullName: 'Tighnari',
+    rating: 5,
+    weapon: 'bow',
+    vision: 'dendro',
+    specialized: 'elemental'
+  },
+  {
+    name: 'venti',
+    fullName: 'Venti',
+    rating: 5,
+    weapon: 'bow',
+    vision: 'anemo',
+    specialized: 'energy'
+  },
+  {
+    name: 'wanderer',
+    fullName: 'Wanderer',
+    rating: 5,
+    weapon: 'catalyst',
+    vision: 'anemo',
+    specialized: 'critrate'
+  },
+  {
+    name: 'xiangling',
+    fullName: 'Xiangling',
+    rating: 4,
+    weapon: 'polearm',
+    vision: 'pyro',
+    specialized: 'em'
+  },
+  {
+    name: 'xiao',
+    fullName: 'Xiao',
+    rating: 5,
+    weapon: 'polearm',
+    vision: 'anemo',
+    specialized: 'critrate'
+  },
+  {
+    name: 'xingqiu',
+    fullName: 'Xingqiu',
+    rating: 4,
+    weapon: 'sword',
+    vision: 'hydro',
+    specialized: 'atk%'
+  },
+  {
+    name: 'xinyan',
+    fullName: 'Xinyan',
+    rating: 4,
+    weapon: 'claymore',
+    vision: 'pyro',
+    specialized: 'atk%'
+  },
+  {
+    name: 'yaemiko',
+    fullName: 'Yae Miko',
+    rating: 5,
+    weapon: 'catalyst',
+    vision: 'electro',
+    specialized: 'critrate'
+  },
+  {
+    name: 'yanfei',
+    fullName: 'Yanfei',
+    rating: 4,
+    weapon: 'catalyst',
+    vision: 'pyro',
+    specialized: 'elemental'
+  },
+  {
+    name: 'yelan',
+    fullName: 'Yelan',
+    rating: 5,
+    weapon: 'bow',
+    vision: 'hydro',
+    specialized: 'critrate'
+  },
+  {
+    name: 'yoimiya',
+    fullName: 'Yoimiya',
+    rating: 5,
+    weapon: 'bow',
+    vision: 'pyro',
+    specialized: 'critrate'
+  },
+  {
+    name: 'yunjin',
+    fullName: 'Yunjin',
+    rating: 4,
+    weapon: 'polearm',
+    vision: 'geo',
+    specialized: 'energy'
+  },
+  {
+    name: 'zhongli',
+    fullName: 'Zhongli',
+    rating: 5,
+    weapon: 'polearm',
+    vision: 'geo',
+    specialized: 'elemental'
+  }
+];
