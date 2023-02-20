01a919d19e29f385887917cec94d4936830fc938
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-02-19 20:03:05 -0400

    LevelGroup design finished

---
+++ src/components/Modal/_LevelGroup.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let label: string = 'Level';
+  export let value: string = '80/90';
+</script>
+
+<div>
+  <div class="mb-1 flex justify-between px-0.5 text-sm">
+    <span>{label}</span>
+    <span>{value}</span>
+  </div>
+  <div class="flex h-10 overflow-hidden rounded-lg bg-slate-800">
+    <button class="w-full">-</button>
+    <div class="h-full w-[1px] bg-slate-500" />
+    <button class="w-full ">+</button>
+  </div>
+</div>
