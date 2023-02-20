dc3f6380b8e74e83943dcb7b4c56fc7aef2d71bc
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-02-19 20:30:33 -0400

    Added filters to Character Modal

--- src/components/Modal/Pages/Character.svelte
+++ src/components/Modal/Pages/Character.svelte
@@ -2,21 +2,25 @@
   import Thumbnail from '../../Thumbnail/Thumbnail.svelte';
   import LevelGroup from '../_LevelGroup.svelte';
   import StarGroup from '../../Stars/StarGroup.svelte';
+  import Filters from '../../Filters/Filters.svelte';
 </script>
 
-<div class=" grid grid-cols-3 gap-x-2 gap-y-3">
-  <Thumbnail img="/images/character/wanderer.webp" vision="anemo" alt="wanderer" />
-  <div class="col-span-2 flex flex-col justify-end">
-    <div>
-      <h3 class="text-lg">Sangonomiya Kokomi</h3>
-      <StarGroup rating={5} />
+<div>
+  <div class=" mb-4 grid grid-cols-3 gap-x-2 gap-y-3">
+    <Thumbnail img="/images/character/wanderer.webp" vision="anemo" alt="wanderer" />
+    <div class="col-span-2 flex flex-col justify-end">
+      <div>
+        <h3 class="text-lg">Sangonomiya Kokomi</h3>
+        <StarGroup rating={5} />
+      </div>
+      <div class="grid grid-cols-2 gap-x-2">
+        <LevelGroup label="Level" value="80/90" />
+        <LevelGroup label="Const." value="0/6" />
+      </div>
     </div>
-    <div class="grid grid-cols-2 gap-x-2">
-      <LevelGroup label="Level" value="80/90" />
-      <LevelGroup label="Const." value="0/6" />
-    </div>
+    <LevelGroup label="Normal" value="10" />
+    <LevelGroup label="Skill" value="10" />
+    <LevelGroup label="Burst" value="13" />
   </div>
-  <LevelGroup label="Normal" value="10" />
-  <LevelGroup label="Skill" value="10" />
-  <LevelGroup label="Burst" value="13" />
+  <Filters selected="anemo" />
 </div>
