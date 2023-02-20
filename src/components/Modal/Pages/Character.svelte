26436a7ac399fe938336b7c5f3a52480feaeacf7
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-02-19 20:08:33 -0400

    Created top half of Character Modal

--- src/components/Modal/Pages/Character.svelte
+++ src/components/Modal/Pages/Character.svelte
@@ -1,7 +1,22 @@
-<p>
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius accusamus esse ad,
-  officia mollitia perspiciatis? Nam vitae error beatae ex veniam eius eveniet, voluptatum
-  magni velit ullam, illum blanditiis vel voluptas ea suscipit, distinctio non culpa
-  praesentium ipsam molestiae voluptatibus quis laborum excepturi. Recusandae quam aliquid
-  optio accusantium dolorum nesciunt!
-</p>
+<script>
+  import Thumbnail from '../../Thumbnail/Thumbnail.svelte';
+  import LevelGroup from '../_LevelGroup.svelte';
+  import StarGroup from '../../Stars/StarGroup.svelte';
+</script>
+
+<div class=" grid grid-cols-3 gap-x-2 gap-y-3">
+  <Thumbnail img="/images/character/wanderer.webp" vision="anemo" alt="wanderer" />
+  <div class="col-span-2 flex flex-col justify-end">
+    <div>
+      <h3 class="text-lg">Sangonomiya Kokomi</h3>
+      <StarGroup rating={5} />
+    </div>
+    <div class="grid grid-cols-2 gap-x-2">
+      <LevelGroup label="Level" value="80/90" />
+      <LevelGroup label="Const." value="0/6" />
+    </div>
+  </div>
+  <LevelGroup label="Normal" value="10" />
+  <LevelGroup label="Skill" value="10" />
+  <LevelGroup label="Burst" value="13" />
+</div>
