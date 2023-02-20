3252899a76d715b5d3f36983a992d399f46a236f
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-02-20 10:49:39 -0400

    Finished character picker component

---
+++ src/components/Picker/Picker.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Thumbnail from '../Thumbnail/Thumbnail.svelte';
+  // props
+  export let data: any;
+  export let type: string;
+  export let h: number;
+  console.log(h);
+</script>
+
+<div
+  style="height: {h}px;"
+  class="mt-2 grid auto-rows-min grid-cols-4 gap-2 overflow-y-auto rounded-2xl border border-slate-600 p-2"
+>
+  {#each data as item}
+    <button on:click={() => console.log(item.name)}>
+      <Thumbnail
+        alt={item.fullName}
+        img="/images/{type}/{item.name}.webp"
+        vision={type === 'character' ? item.vision : undefined}
+      />
+      <p class="mt-1 overflow-hidden text-ellipsis whitespace-nowrap text-sm font-medium">
+        {item.fullName}
+      </p>
+    </button>
+  {/each}
+</div>
