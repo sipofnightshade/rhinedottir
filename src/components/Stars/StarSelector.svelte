4a19f7cd189cd0087a2c43401ac1a89c2cb936ab
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-02-21 20:47:09 -0400

    StarSelector design finished

---
+++ src/components/Stars/StarSelector.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Star from './Star.svelte';
+</script>
+
+<div class="mb-2 flex h-10 items-center justify-between rounded-lg bg-slate-800 p-3">
+  <Star />
+  <Star />
+  <Star />
+  <Star />
+  <Star />
+</div>
