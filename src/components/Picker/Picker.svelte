1c03d03c63914f731dafa33f5fa7af1a9fcdb879
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-02-27 22:21:13 -0400

    ✅Added logic and state to Picker

--- src/components/Picker/Picker.svelte
+++ src/components/Picker/Picker.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
+  import { createEventDispatcher } from 'svelte';
   import Thumbnail from '../Thumbnail/Thumbnail.svelte';
+  import type { SelectedCharacter, SelectedWeapon } from '$lib/types/global';
   // props
   export let data: any;
   export let type: string;
   export let h: number;
+
+  const dispatch = createEventDispatcher();
+
+  function handleClick(item: SelectedCharacter | SelectedWeapon) {
+    dispatch('selected', {
+      selected: item
+    });
+  }
 </script>
 
 <div
@@ -11,7 +21,7 @@
   class="mt-2 grid auto-rows-min grid-cols-4 gap-2 overflow-y-auto rounded-2xl border border-slate-600 p-2"
 >
   {#each data as item}
-    <button on:click={() => console.log(item.name)}>
+    <button on:click={() => handleClick(item)}>
       <Thumbnail
         alt={item.fullName}
         img="/images/{type}/{item.name}.webp"
