119f91f036cb1975db69dbd802c8eef12f7ba213
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-02-27 22:10:10 -0400

    ✅Added logic and state to levelgroup

--- src/components/Modal/Character/Character.svelte
+++ src/components/Modal/Character/Character.svelte
@@ -1,11 +1,23 @@
-<script>
+<script lang="ts">
   import Thumbnail from '../../Thumbnail/Thumbnail.svelte';
   import LevelGroup from '../_LevelGroup.svelte';
   import StarGroup from '../../Stars/StarGroup.svelte';
   import Filters from '../../Filters/Filters.svelte';
   import Picker from '../../Picker/Picker.svelte';
-
   import { CharacterData } from '$lib/data/Characters';
+  import { character } from '$lib/stores/characterStore';
+  import { labels } from '$lib/data/Levels';
+
+  // // store
+  $character;
+
+  function handleIncrement(event: any) {
+    character.increment(event.detail.groupID);
+  }
+
+  function handleDecrement(event: any) {
+    character.decrement(event.detail.groupID);
+  }
 
   let profileH;
   let contentH;
@@ -13,20 +25,54 @@
 
 <div class="h-full overflow-hidden" bind:clientHeight={contentH}>
   <div bind:clientHeight={profileH} class=" mb-4 grid grid-cols-3 gap-x-2 gap-y-3">
-    <Thumbnail img="/images/character/wanderer.webp" vision="anemo" alt="wanderer" />
+    <Thumbnail
+      img="/images/character/{$character.selected.name}.webp"
+      vision={$character.selected.vision}
+      alt={$character.selected.fullName}
+    />
     <div class="col-span-2 flex flex-col justify-end">
       <div>
-        <h3 class="text-lg">Wanderer</h3>
-        <StarGroup rating={5} />
+        <h3 class="text-lg">{$character.selected.fullName}</h3>
+        <StarGroup rating={$character.selected.rating} />
       </div>
       <div class="grid grid-cols-2 gap-x-2">
-        <LevelGroup label="Level" value="80/90" />
-        <LevelGroup label="Const." value="0/6" />
+        <LevelGroup
+          label="Level"
+          value={labels.lvl[$character.lvl]}
+          id="lvl"
+          on:increment={handleIncrement}
+          on:decrement={handleDecrement}
+        />
+        <LevelGroup
+          label="Const."
+          value={labels.constellation[$character.constellation]}
+          id="constellation"
+          on:increment={handleIncrement}
+          on:decrement={handleDecrement}
+        />
       </div>
     </div>
-    <LevelGroup label="Normal" value="10" />
-    <LevelGroup label="Skill" value="10" />
-    <LevelGroup label="Burst" value="13" />
+    <LevelGroup
+      label="Normal"
+      value={labels.atk[$character.atk]}
+      id="atk"
+      on:increment={handleIncrement}
+      on:decrement={handleDecrement}
+    />
+    <LevelGroup
+      label="Skill"
+      value={labels.skill[$character.skill]}
+      id="skill"
+      on:increment={handleIncrement}
+      on:decrement={handleDecrement}
+    />
+    <LevelGroup
+      label="Burst"
+      value={labels.burst[$character.burst]}
+      id="burst"
+      on:increment={handleIncrement}
+      on:decrement={handleDecrement}
+    />
   </div>
   <div class="h-full">
     <Filters selected="anemo" />
--- src/components/Modal/_LevelGroup.svelte
+++ src/components/Modal/_LevelGroup.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  // props
   export let label: string;
   export let value: string | number;
+  export let id: string;
+
+  const dispatch = createEventDispatcher();
+
+  function decreaseValue() {
+    dispatch('decrement', {
+      groupID: id
+    });
+  }
+
+  function increaseValue() {
+    dispatch('increment', {
+      groupID: id
+    });
+  }
+
   // export let max:number; // maximum input value
 </script>
 
@@ -10,8 +28,8 @@
     <span>{value}</span>
   </div>
   <div class="flex h-10 overflow-hidden rounded-lg bg-slate-800">
-    <button class="w-full">-</button>
+    <button on:click={decreaseValue} class="w-full">-</button>
     <div class="h-full w-[1px] bg-slate-500" />
-    <button class="w-full ">+</button>
+    <button on:click={increaseValue} class="w-full ">+</button>
   </div>
 </div>
