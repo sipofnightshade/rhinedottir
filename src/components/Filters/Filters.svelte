8ce8a5679e75f116c2fd2316bb887cb23a242dde
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-02-27 23:14:14 -0400

    ✅Finishied and made Filters more reusable

--- src/components/Filters/Filters.svelte
+++ src/components/Filters/Filters.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
-  //   export let filters: string[];
+  import { createEventDispatcher } from 'svelte';
+  export let filters: string[];
   export let selected: string;
-  const filters = ['anemo', 'cryo', 'dendro', 'electro', 'geo', 'hydro', 'pyro'];
+
+  const dispatch = createEventDispatcher();
+
+  function setFilter(value: string) {
+    dispatch('filter', { selected: value });
+  }
 </script>
 
 <div class="grid w-full auto-cols-fr grid-flow-col gap-2">
   {#each filters as filter}
     <button
+      on:click={() => setFilter(filter)}
       class="relative flex aspect-square items-center justify-center rounded-lg"
-      class:bg-slate-500={selected === filter}
+      class:bg-slate-500={filter === selected}
     >
       <img src="/images/elements/{filter}.svg" alt={filter} class="w-8" />
     </button>
