95031f7542e51a2257d34e265b4867d8af568ded
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-03-03 22:17:29 -0400

    StatRows and StatsRow working perfectly

--- src/components/TableRow/StatRow.svelte
+++ src/components/TableRow/StatRow.svelte
@@ -1,20 +1,27 @@
 <script lang="ts">
   import Cell from './Cell.svelte';
-  export let head = false;
+  export let label: string;
+  export let base: number = 0;
+  export let total: number;
+  export let isPcnt: boolean;
+
+  const numFormat = Intl.NumberFormat('en-US');
+  $: displayBase =
+    base === 0
+      ? '0'
+      : isPcnt
+      ? total.toFixed(1) + '%'
+      : numFormat.format(+base.toFixed(0));
+  $: displayTotal =
+    total === 0
+      ? '0'
+      : isPcnt
+      ? total.toFixed(1) + '%'
+      : numFormat.format(+total.toFixed(0));
 </script>
 
-{#if head}
-  <div
-    class="mb-.5 grid grid-cols-20 border-b border-slate-500 px-0.5 pb-1.5 text-sm font-bold uppercase"
-  >
-    <Cell align="start" col="col-span-12" value="Stat" />
-    <Cell align="end" col="col-span-4" value="Base" />
-    <Cell align="end" col="col-span-4" value="Total" />
-  </div>
-{:else}
-  <div class="grid grid-cols-20 px-0.5 py-1 text-tb">
-    <Cell align="start" col="col-span-12" value="Elemental Mastery" />
-    <Cell align="end" col="col-span-4" value="12,276" />
-    <Cell align="end" col="col-span-4" value="19,945" />
-  </div>
-{/if}
+<div class="grid grid-cols-20 px-0.5 py-1 text-tb">
+  <Cell align="start" col="col-span-12" value={label} />
+  <Cell align="end" col="col-span-4" value={displayBase} />
+  <Cell align="end" col="col-span-4" value={displayTotal} />
+</div>
