35031492c6a6e25461c5f3aebc23ea0f5198c772
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-03-04 21:22:13 -0400

    Created first test file: genshinStats script test

---
+++ src/lib/helpers/genshinStatsAll.test.ts
@@ -0,0 +1,38 @@
+import genshinStatsAll from './genshinStatsAll';
+import { CharacterData } from '$lib/data/Characters';
+import { WeaponData } from '$lib/data/Weapons';
+import { labels } from '$lib/data/Levels';
+
+describe('Test genshinStatsAll script', () => {
+  test('returns character stats', () => {
+    CharacterData.forEach((character) => {
+      labels.lvlValues.forEach((level) => {
+        const result = genshinStatsAll.calcStatsForCharacter(character.name, level);
+        expect(result).toHaveProperty('level');
+        expect(result).toHaveProperty('ascension');
+        expect(result).toHaveProperty('hp');
+        expect(result).toHaveProperty('attack');
+        expect(result).toHaveProperty('defense');
+        expect(result).toHaveProperty('specialized');
+        expect(result).toHaveProperty('substat');
+
+        // Add more expect statements to test the result here
+      });
+    });
+  });
+
+  test('returns weapon stats', () => {
+    WeaponData.sword.forEach((weapon) => {
+      labels.lvlValues.forEach((level) => {
+        const result = genshinStatsAll.calcStatsForWeapon(weapon.name, level);
+        expect(result).toHaveProperty('level');
+        expect(result).toHaveProperty('ascension');
+        expect(result).toHaveProperty('attack');
+        expect(result).toHaveProperty('specialized');
+        expect(result).toHaveProperty('substat');
+
+        // Add more expect statements to test the result here
+      });
+    });
+  });
+});
