9d46b2249eec170938cad66ce7809c1e5ec470b8
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-03-05 15:00:11 -0400

    ✅Damage Table and TalentRows working

--- src/components/Table/Damage.svelte
+++ src/components/Table/Damage.svelte
@@ -1,26 +1,70 @@
-<script>
+<script lang="ts">
   import TalentRow from '../TableRow/TalentRow.svelte';
+  import Cell from '../TableRow/Cell.svelte';
+
+  import { Talents } from '$lib/data/Talents';
+  import { stats } from '$lib/stores/statsStore';
+  import { character } from '$lib/stores/characterStore';
+
+  const charName = 'traveleranemo';
+  const infusion = undefined;
+
+  $: talentRows = Talents[charName];
 </script>
 
 <div class="">
-  <TalentRow head={true} />
-  <TalentRow />
-  <TalentRow />
-  <TalentRow />
-  <TalentRow />
-  <TalentRow />
-  <TalentRow />
-  <TalentRow />
-  <TalentRow />
-  <TalentRow />
-  <TalentRow />
-  <TalentRow />
-  <TalentRow />
-  <TalentRow />
-  <TalentRow />
-  <TalentRow />
-  <TalentRow />
-  <TalentRow />
-  <TalentRow />
-  <TalentRow />
+  <div
+    class="mb-1.5 grid grid-cols-24 border-b border-slate-500 px-0.5 pb-1.5 text-sm font-bold uppercase"
+  >
+    <Cell align="start" col="col-span-2" value="ID" />
+    <Cell align="start" col="col-span-9" value="Talent" />
+    <Cell align="center" col="col-span-3" value="-" />
+    <Cell align="end" col="col-span-4" value="Base" />
+    <Cell align="end" col="col-span-6" value="Total" />
+  </div>
+
+  <div class=" grid grid-cols-24 rounded-sm bg-slate-600 py-1 px-1.5 text-tb">
+    <Cell align="start" col="col-span-full" value={talentRows.normal.name} />
+  </div>
+
+  {#each talentRows.normal.hits as hit}
+    <TalentRow
+      type={'combat1'}
+      data={hit}
+      stats={$stats}
+      charLvl={$character.lvl}
+      talentLvl={$character.atk}
+      el={infusion ?? 'physical'}
+    />
+  {/each}
+
+  <div class="my-1 grid grid-cols-24 rounded-sm bg-slate-600 py-1 px-1.5 text-tb">
+    <Cell align="start" col="col-span-full" value={talentRows.skill.name} />
+  </div>
+
+  {#each talentRows.skill.hits as hit}
+    <TalentRow
+      type={'combat2'}
+      data={hit}
+      stats={$stats}
+      charLvl={$character.lvl}
+      talentLvl={$character.skill}
+      el={$character.selected.vision}
+    />
+  {/each}
+
+  <div class="my-1 grid grid-cols-24 rounded-sm bg-slate-600 py-1 px-1.5 text-tb">
+    <Cell align="start" col="col-span-full" value={talentRows.burst.name} />
+  </div>
+
+  {#each talentRows.burst.hits as hit}
+    <TalentRow
+      type={'combat3'}
+      data={hit}
+      stats={$stats}
+      charLvl={$character.lvl}
+      talentLvl={$character.burst}
+      el={$character.selected.vision}
+    />
+  {/each}
 </div>
--- src/components/TableRow/TalentRow.svelte
+++ src/components/TableRow/TalentRow.svelte
@@ -1,26 +1,70 @@
 <script lang="ts">
+  import type { Hit } from '$lib/data/TalentsTypes';
+  import type { DamageType } from '$lib/types/global';
+  import TalentValues from '$lib/data/TalentValues.json';
   import Cell from './Cell.svelte';
-  export let head = false;
+
+  // import calculators
+  import { calcDEFMultiplier } from '$lib/calculators/calcDEFMultiplier';
+  import { calcRESMultiplier } from '$lib/calculators/calcRESMultiplier';
+  import { calcCritDamage } from '$lib/calculators/calcCritDamage';
+  import { calcSpecialMultiplier } from '$lib/calculators/calcSpecialMultiplier';
+  import { calcDamage } from '$lib/calculators/calcDamage';
+
+  export let type: 'combat1' | 'combat2' | 'combat3';
+  export let data: Hit;
+  export let stats: any;
+  export let charLvl: number;
+  export let talentLvl: number;
+  export let el: DamageType;
+
+  // temporary values
+  const values = TalentValues.traveleranemo[type];
+  const defIgnore = 0;
+  const defReduce = 0;
+  const baseRes = 0.1;
+  const bonusRes = 0;
+  const debuffRes = 0;
+
+  // talent element
+  const element = data.elemental ? data.elemental : el;
+
+  // character damage values and multipliers
+  // const SpecialMultiplier = calcSpecialMultiplier(buffs.special);
+  const SpecialMultiplier = 1;
+  const FlatDMG = 0;
+  const DMGBonus = 0;
+  const AmplifyingMultipler = 1;
+
+  // target stats
+  const DMGReduction = 0;
+  const DEFMultiplier = calcDEFMultiplier(charLvl, 100, defReduce, defIgnore);
+  const RESMultiplier = calcRESMultiplier(baseRes, bonusRes, debuffRes);
+
+  $: FinalDMG = data.damage.reduce((total, damage) => {
+    const BaseDMG =
+      stats[damage.scaling] * values[damage.param as keyof typeof values][talentLvl];
+
+    const calculatedDMG = calcDamage(
+      BaseDMG,
+      SpecialMultiplier,
+      FlatDMG,
+      DMGBonus,
+      DMGReduction,
+      DEFMultiplier,
+      RESMultiplier,
+      AmplifyingMultipler
+    );
+    return total + calculatedDMG;
+  }, 0);
 </script>
 
-{#if head}
-  <div
-    class="mb-.5 grid grid-cols-24 border-b border-slate-500 px-0.5 pb-1.5 text-sm font-bold uppercase"
-  >
-    <Cell align="start" col="col-span-2" value="ID" />
-    <Cell align="start" col="col-span-9" value="Talent" />
-    <Cell align="center" col="col-span-3" value="-" />
-    <Cell align="end" col="col-span-4" value="Base" />
-    <Cell align="end" col="col-span-6" value="Total" />
-  </div>
-{:else}
-  <div class="grid grid-cols-24 px-0.5 py-1 text-tb">
-    <Cell align="start" col="col-span-2" value="N1" />
-    <Cell align="start" col="col-span-9" value="1 - hit DMG" />
-    <Cell align="center" col="col-span-3">
-      <img class="h-6 w-6" src="/images/elements/anemo.svg" alt="" />
-    </Cell>
-    <Cell align="end" col="col-span-4" value="31,040" />
-    <Cell align="end" col="col-span-6" value="121,000" />
-  </div>
-{/if}
+<div class="grid grid-cols-24 px-1 py-1 text-tb">
+  <Cell align="start" col="col-span-2" value={data.tag} />
+  <Cell align="start" col="col-span-10 text-slate-400" value={data.name} />
+  <Cell align="center" col="col-span-2">
+    <img class="h-6 w-6" src="/images/elements/{el}.svg" alt="" />
+  </Cell>
+  <Cell align="end" col="col-span-4 text-slate-400" value={FinalDMG.toFixed(0)} />
+  <Cell align="end" col="col-span-6" value={(FinalDMG * 1.5).toFixed(0)} />
+</div>
