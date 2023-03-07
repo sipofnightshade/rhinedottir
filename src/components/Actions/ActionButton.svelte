fd35fced22cd29d6328699e81baac13c346cdec5
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-03-07 19:55:53 -0400

    Added typescript to ActionButton

--- src/components/Actions/ActionButton.svelte
+++ src/components/Actions/ActionButton.svelte
@@ -1,49 +1,50 @@
-<script>
-	/**
-	 * @type {"anemo"|"cryo"|"dendro"|"electro"|"hydro"|"geo"|"pyro"}
-	 */
-	export let element = 'anemo';
-	let isActive = true;
+<script lang="ts">
+  import type { Visions } from '$lib/types/global';
+  import type { Action } from '$lib/types/talents';
 
-	const backgrounds = {
-		anemo: 'bg-anemo',
-		cryo: 'bg-cryo',
-		dendro: 'bg-dendro',
-		electro: 'bg-electro',
-		hydro: 'bg-hydro',
-		geo: 'bg-geo',
-		pyro: 'bg-pyro'
-	};
+  // props
+  export let element: Visions = 'anemo';
+  export let isActive = true;
 
-	const outlines = {
-		anemo: 'outline-anemo',
-		cryo: 'outline-cryo',
-		dendro: 'outline-dendro',
-		electro: 'outline-electro',
-		hydro: 'outline-hydro',
-		geo: 'outline-geo',
-		pyro: 'outline-pyro'
-	};
+  const backgrounds = {
+    anemo: 'bg-anemo',
+    cryo: 'bg-cryo',
+    dendro: 'bg-dendro',
+    electro: 'bg-electro',
+    hydro: 'bg-hydro',
+    geo: 'bg-geo',
+    pyro: 'bg-pyro'
+  };
+  const outlines = {
+    anemo: 'outline-anemo',
+    cryo: 'outline-cryo',
+    dendro: 'outline-dendro',
+    electro: 'outline-electro',
+    hydro: 'outline-hydro',
+    geo: 'outline-geo',
+    pyro: 'outline-pyro'
+  };
 </script>
 
-<button
-	class="relative mr-1 flex h-16 w-16 flex-shrink-0 items-center justify-center grayscale-80"
-	class:filter-none={isActive}
-	class:opacity-60={!isActive}
+<div
+  data-testid="action-button"
+  class="relative flex h-16 w-16 flex-shrink-0 items-center justify-center grayscale-80"
+  class:filter-none={isActive}
+  class:opacity-60={!isActive}
 >
-	<div
-		class="absolute h-12 w-12 rounded-full {backgrounds[element]} opacity-0 blur-md"
-		class:opacity-50={isActive}
-	/>
-	<div class="absolute h-[46px] w-[46px] rotate-45 rounded-sm {backgrounds[element]}" />
-	<div
-		class="absolute h-[52px] w-[52px] rounded-full {backgrounds[
-			element
-		]} outline outline-1 {outlines[element]}"
-	/>
-	<img
-		class="z-10 h-[52px] w-[52px] rounded-full border-2 border-slate-200 "
-		src="/images/talents/UI_Talent_S_Xiao_02.png"
-		alt="Skill Button Xiao"
-	/>
-</button>
+  <div
+    class="absolute h-12 w-12 rounded-full {backgrounds[element]} opacity-0 blur-md"
+    class:opacity-50={isActive}
+  />
+  <div class="absolute h-[46px] w-[46px] rotate-45 rounded-sm {backgrounds[element]}" />
+  <div
+    class="absolute h-[52px] w-[52px] rounded-full {backgrounds[
+      element
+    ]} outline outline-1 {outlines[element]}"
+  />
+  <img
+    class="z-10 h-[52px] w-[52px] rounded-full border-2 border-slate-200 "
+    src="/images/talents/UI_Talent_S_Xiao_02.png"
+    alt="Skill Button Xiao"
+  />
+</div>
