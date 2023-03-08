b13588df620f1538297af8d9c46a44891f0e6c81
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-03-08 08:44:04 -0400

    Added actionModal to StackButton but disabled it

--- src/components/Actions/_StackButton.svelte
+++ src/components/Actions/_StackButton.svelte
@@ -3,10 +3,12 @@
   import type { Visions } from '$lib/types/global';
   import ActionButton from './ActionButton.svelte';
   import { action } from '$lib/stores/actionStore';
+  import ActionModal from '../ActionModal/ActionModal.svelte';
 
   export let element: Visions;
   export let data: Action;
 
+  // let showModal = true;
   let stacks = 0;
 
   function handleToggle() {
@@ -28,19 +30,47 @@
       console.log('Store', $action);
     }
   }
+
+  // function toggleModal() {
+  //   showModal = !showModal;
+  // }
+
+  // function closeModal() {
+  //   showModal = false;
+  // }
 </script>
 
 <button on:click={handleToggle} class="relative shadow-red-300">
   <ActionButton {element} isActive={stacks > 0} />
   {#if stacks > 0}
     <p
-      class="stacks absolute top-0 right-0 z-30 text-lg font-bold text-anemo shadow-red-400"
+      class="stacks absolute top-0 right-0 z-10 text-lg font-bold text-anemo shadow-red-400"
     >
       x{stacks}
     </p>
   {/if}
 </button>
 
+<!-- {#if showModal}
+  <ActionModal
+    on:click={closeModal}
+    on:escapeClick={closeModal}
+    modalTitle={data.name}
+    actionType="Elemental Burst"
+    buttonType="Stacks"
+    details={data.description}
+  >
+    <div class="flex items-center space-x-2">
+      <input
+        type="number"
+        max={data.values.length}
+        min={0}
+        class="h-8 w-24 rounded bg-slate-600 text-right"
+      />
+      <p class="text-slate-400">/ {data.values.length}</p>
+    </div>
+  </ActionModal>
+{/if} -->
 <style lang="postcss">
   .stacks {
     text-shadow: -1px -1px 2px #334155, 1px -1px 2px #334155, -1px 1px 1px #334155,
