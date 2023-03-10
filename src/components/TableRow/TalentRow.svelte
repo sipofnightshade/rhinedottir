fafcd189d1274cab8214cf333f57957ae7e3dc33
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-03-10 11:59:06 -0400

    ✅Fixed TalentRow tag showing undefined

--- src/components/TableRow/TalentRow.svelte
+++ src/components/TableRow/TalentRow.svelte
@@ -8,10 +8,10 @@
 </script>
 
 <div class="grid grid-cols-24 px-1 py-1 text-tb">
-  <Cell align="start" col="col-span-2" value={data.tag} />
+  <Cell align="start" col="col-span-2" value={data.tag || '-'} />
   <Cell align="start" col="col-span-10 text-slate-400" value={data.name} />
   <Cell align="center" col="col-span-2">
-    <img class="h-6 w-6" src="/images/elements/{el}.svg" alt="" />
+    <img class="h-6 w-6" src="/images/elements/{el}.svg" alt={el} />
   </Cell>
   <Cell align="end" col="col-span-4 text-slate-400" value={data.damage.toFixed(0)} />
   <Cell align="end" col="col-span-6" value={(data.damage * 1.5).toFixed(0)} />
