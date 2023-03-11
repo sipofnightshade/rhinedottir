6eea95c0d547104c5815afd4d8f9b428e217be07
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-03-11 10:36:44 -0400

    Added gradients to ActionButton bg

--- src/components/Actions/ActionButton.svelte
+++ src/components/Actions/ActionButton.svelte
@@ -15,6 +15,17 @@
     geo: 'bg-geo',
     pyro: 'bg-pyro'
   };
+
+  const gradients = {
+    anemo: 'bg-gradient-to-b from-anemo to-anemoDark',
+    cryo: 'bg-gradient-to-b from-cryo to-cryoDark',
+    dendro: 'bg-gradient-to-b from-dendro to-dendroDark',
+    electro: 'bg-gradient-to-b from-electro to-electroDark',
+    hydro: 'bg-gradient-to-b from-hydro to-hydroDark',
+    geo: 'bg-gradient-to-b from-geo to-geoDark',
+    pyro: 'bg-gradient-to-b from-pyro to-pyroDark'
+  };
+
   const outlines = {
     anemo: 'outline-anemo',
     cryo: 'outline-cryo',
@@ -30,7 +41,7 @@
   data-testid="action-button"
   class="relative flex h-16 w-16 flex-shrink-0 items-center justify-center grayscale-80"
   class:filter-none={isActive}
-  class:opacity-60={!isActive}
+  class:opacity-50={!isActive}
 >
   <div
     class="absolute h-12 w-12 rounded-full {backgrounds[element]} opacity-0 blur-md"
@@ -38,7 +49,7 @@
   />
   <div class="absolute h-[46px] w-[46px] rotate-45 rounded-sm {backgrounds[element]}" />
   <div
-    class="absolute h-[52px] w-[52px] rounded-full {backgrounds[
+    class="absolute h-[52px] w-[52px] rounded-full {gradients[
       element
     ]} outline outline-1 {outlines[element]}"
   />
