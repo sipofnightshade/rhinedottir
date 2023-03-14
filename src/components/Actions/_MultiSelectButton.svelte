a56d8026d69e7ba83866dc721cc9a017542242ca
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-03-14 09:53:04 -0400

    New MultiSelect button logic working

--- src/components/Actions/_MultiSelectButton.svelte
+++ src/components/Actions/_MultiSelectButton.svelte
@@ -6,36 +6,28 @@
   import type { ALL_STATS } from '$lib/types/talents';
   import { action } from '$lib/stores/actionStore';
   import { stripStat } from '$lib/helpers/stripStats';
+  import { onMount } from 'svelte';
 
   export let element: Visions;
   export let data: Action;
 
-  let selectedStats: { scaling: ALL_STATS; coef: number }[] = [];
-  let prevStats: { scaling: ALL_STATS; coef: number }[] = [];
+  type SELECTED = { [key in ALL_STATS]?: boolean };
 
-  // function clearState() {
-  //   if (selectedStats) {
-  //     // selectedStats.forEach((stat) => {
-  //     //   action.removeStat(stat.scaling, stat.coef);
-  //     // });
-  //     console.log('ClearState:', selectedStats);
-  //     selectedStats = [];
-  //   }
-  // }
+  let selectedStats: SELECTED = {};
 
-  $: {
-    if (selectedStats && selectedStats.length > prevStats.length) {
-      let latestStat = selectedStats[selectedStats.length - 1];
-      // add latest stat to store
-      action.addStat(latestStat.scaling, latestStat.coef);
-    } else if (selectedStats && selectedStats.length < prevStats.length) {
-      // remove stat from the store
+  onMount(() => {
+    data.values.forEach(({ scaling }) => {
+      selectedStats[scaling] = false;
+    });
+  });
 
-      let removedStat = prevStats.filter((stat) => !selectedStats.includes(stat))[0];
-      action.removeStat(removedStat.scaling, removedStat.coef);
+  function handleClick(stat: { scaling: ALL_STATS; coef: number }) {
+    selectedStats[stat.scaling] = !selectedStats[stat.scaling];
+    if (selectedStats[stat.scaling] === true) {
+      action.addStat(stat.scaling, stat.coef);
+    } else {
+      action.removeStat(stat.scaling, stat.coef);
     }
-
-    prevStats = selectedStats ? [...selectedStats] : [];
   }
 
   // handle Modal
@@ -43,23 +35,27 @@
   function toggleModal() {
     showModal = !showModal;
   }
+  console.log(selectedStats);
+
+  // $: console.log('Selected', selectedStats);
 </script>
 
 <button on:click={toggleModal} class="relative">
-  <ActionButton {element} isActive={selectedStats.length > 0} />
-  {#if selectedStats.length > 0}
-    <div class="absolute bottom-0 right-0 z-10 flex -space-x-2.5">
-      {#each selectedStats as stat}
-        <div class="rounded-full bg-slate-800 p-1">
-          <img
-            class="w-4"
-            src="/images/elements/{stripStat(stat.scaling)}.svg"
-            alt="close"
-          />
-        </div>
-      {/each}
-    </div>
-  {/if}
+  <ActionButton {element} isActive={true} />
+  <div class="absolute bottom-0 right-0 z-10 flex -space-x-2.5">
+    {#each data.values as value}
+      <div
+        class="rounded-full bg-slate-800 p-1"
+        class:hidden={selectedStats[value.scaling] === false}
+      >
+        <img
+          class="w-4"
+          src="/images/elements/{stripStat(value.scaling)}.svg"
+          alt="close"
+        />
+      </div>
+    {/each}
+  </div>
 </button>
 {#if showModal}
   <ActionModal
@@ -67,8 +63,8 @@
     on:escapeClick={toggleModal}
     modalTitle={data.name}
     actionType="Elemental Burst"
-    buttonType="Select"
-    details={data.description}
+    buttonType="Multi-Select"
+    details={'hi'}
   >
     <ul class="flex space-x-1" class:bg-red-700={false}>
       <!-- <li class="h-10 w-10">
@@ -79,26 +75,19 @@
           <img class="w-3.5" src="/images/ui/close.svg" alt={'close'} />
         </button>
       </li> -->
-      {#each data.values as stat}
-        <li
-          class="flex h-10 w-10 items-center justify-center rounded-full bg-slate-600"
-          class:bg-slate-600={selectedStats && selectedStats.includes(stat)}
-        >
-          <input
-            type="checkbox"
-            bind:group={selectedStats}
-            name="multiselect"
-            value={stat}
-            class="hidden"
-            id={stat.scaling}
-          />
-          <label for={stat.scaling}>
+      {#each data.values as value}
+        <li>
+          <button
+            class="flex h-10 w-10 items-center justify-center rounded-full bg-slate-600"
+            class:bg-slate-600={selectedStats[value.scaling] === true}
+            on:click={() => handleClick(value)}
+          >
             <img
               class="w-6"
-              src="/images/elements/{stripStat(stat.scaling)}.svg"
-              alt={stripStat(stat.scaling)}
+              src="/images/elements/{stripStat(value.scaling)}.svg"
+              alt={stripStat(value.scaling)}
             />
-          </label>
+          </button>
         </li>
       {/each}
     </ul>
