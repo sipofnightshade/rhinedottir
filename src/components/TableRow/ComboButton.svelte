576c531a4cd231b06495310a7095a99008855669
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-06-04 22:06:39 -0400

    Added new combo button style, not done

--- src/components/TableRow/ComboButton.svelte
+++ src/components/TableRow/ComboButton.svelte
@@ -34,3 +34,25 @@
     {/if}
   </div>
 </button>
+
+<!-- NEW BUTTON -->
+<button
+  class="relative mr-2 flex h-16 w-10 items-center justify-center rounded-lg bg-slate-700 "
+  ><div
+    class="top absolute top-0 z-10 flex h-full w-full justify-center rounded-lg bg-slate-500"
+  >
+    <img class="m-1 h-5 w-5" src="/images/elements/pyro.svg" alt="element" />
+  </div>
+  <img
+    class="absolute bottom-0 m-1 h-5 w-5"
+    src="/images/elements/anemo.svg"
+    alt="element"
+  />
+  <div class="z-[11]  font-bold text-white">N1</div>
+</button>
+
+<style lang="postcss">
+  .top {
+    clip-path: polygon(0 0, 100% 0%, 100% 35%, 0 65%);
+  }
+</style>
