ab7a89cc6b77198d2aa8c03d5e831d34ad8bc8e4
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-06-05 12:18:03 -0400

    Added inline SVG's as svelte components

--- src/components/Desktop/Artifact/Stat.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-</script>
-
-<div class="flex h-full w-full items-center justify-between">
-  <img class="w-4  lg:w-5" src="/images/elements/crit.svg" alt="" />
-  <span class="text-xs lg:text-sm xl:text-base">312.5%</span>
-</div>
---
+++ src/lib/icons/index.ts
@@ -0,0 +1,16 @@
+export { default as Anemo } from './Anemo.svelte';
+export { default as Attack } from './Attack.svelte';
+export { default as CritDamage } from './CritDamage.svelte';
+export { default as CritRate } from './CritRate.svelte';
+export { default as Cryo } from './Cryo.svelte';
+export { default as Defence } from './Defence.svelte';
+export { default as Dendro } from './Dendro.svelte';
+export { default as Electro } from './Electro.svelte';
+export { default as EM } from './EM.svelte';
+export { default as Energy } from './Energy.svelte';
+export { default as Geo } from './Geo.svelte';
+export { default as Healing } from './Healing.svelte';
+export { default as HP } from './HP.svelte';
+export { default as Hydro } from './Hydro.svelte';
+export { default as Physical } from './Physical.svelte';
+export { default as Pyro } from './Pyro.svelte';
