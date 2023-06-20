4cd8b91d59cd46bf02464e118a323a726497a93a
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-06-20 09:01:21 -0400

    Finished new dialog modal ✅

--- src/components/Modal/Modal.svelte
+++ src/components/Modal/Modal.svelte
@@ -1,45 +1,41 @@
 <script lang="ts">
-  // import { createEventDispatcher, onMount } from 'svelte';
-
-  export let modalContent: any;
-  export let modalTitle: string | undefined;
-  export let classes: string;
-
-  // const dispatch = createEventDispatcher();
-
-  // function handleEscape(event: KeyboardEvent) {
-  //   if (event.key === 'Escape') {
-  //     event.stopPropagation();
-  //     dispatch('escapeClick');
-  //   }
-  // }
-
-  // onMount(() => {
-  //   window.addEventListener('keydown', handleEscape);
-  //   return () => {
-  //     window.removeEventListener('keydown', handleEscape);
-  //   };
-  // });
+  export let dialog: HTMLDialogElement;
+  export let title: string = 'Title';
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<div
-  on:click|self|stopPropagation
-  class="fixed top-0 left-0 z-50 flex h-full w-full items-center justify-center bg-slate-900/40"
+<dialog
+  bind:this={dialog}
+  on:click|self={() => dialog.close()}
+  class="h-full items-start rounded-xl border-b border-slate-500 bg-slate-700 p-1 text-slate-200 backdrop:bg-slate-900/75 backdrop:backdrop-blur-sm xs-300:p-2 xs:h-[90vh] xs:w-[452px] xs:rounded-2xl xs:p-4"
 >
-  <div class={classes}>
-    <div
-      class="relative flex w-full items-start justify-center border-b border-slate-500 p-2 xs:p-4"
-    >
-      <h3>{modalTitle}</h3>
-      <button
-        on:click|self|stopPropagation
-        class="absolute right-2 rounded-md bg-anemo px-3 text-slate-200 xs:right-4"
-        >X</button
-      >
+  <div class="flex h-full flex-col items-center rounded-xl bg-slate-700 ">
+    <div class="flex w-full justify-between border-b border-slate-500 p-2 xs:p-4">
+      <h3>{title}</h3>
+      <button on:click={() => dialog.close()}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5 fill-slate-200"
+          viewBox="0 0 384 512"
+        >
+          <path
+            d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
+          />
+        </svg>
+      </button>
     </div>
-    <div class="h-full overflow-hidden p-2 xs:p-4">
-      <svelte:component this={modalContent} />
+    <div class="h-full overflow-hidden p-1 xs-300:p-2 xs:p-4">
+      <slot />
     </div>
   </div>
-</div>
+</dialog>
+
+<style lang="postcss">
+  dialog {
+    padding: 0;
+  }
+  dialog:modal {
+    max-width: 95vw;
+    max-height: 95vh;
+  }
+</style>
