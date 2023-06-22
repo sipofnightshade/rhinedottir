5f85b70fd1ef0e63f2cbc2be365d9251aad624b8
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-06-22 19:31:39 -0400

    Created Combo modal page

---
+++ src/components/Modal/Combo/Button.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let value: string;
+
+  let toggled = false;
+  function toggle() {
+    toggled = !toggled;
+  }
+
+  /**
+   * @todo Add all talents for $currentCharacter as these buttons
+   */
+</script>
+
+<button
+  class="flex w-full items-center justify-center rounded {toggled
+    ? 'bg-slate-100'
+    : 'bg-slate-400'} py-3 text-slate-900"
+  on:click={toggle}
+>
+  {value}
+</button>
---
+++ src/components/Modal/Combo/Combo.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import Button from './Button.svelte';
+</script>
+
+<div>
+  <!-- Normal -->
+  <section class="mb-1 grid grid-flow-col gap-1">
+    <Button value="N1" />
+    <Button value="N2" />
+    <Button value="N3" />
+    <Button value="N4" />
+    <Button value="N5" />
+  </section>
+  <!-- Charged -->
+  <section class="mb-1 grid  grid-flow-col gap-1">
+    <Button value="CA" />
+    <Button value="CF" />
+  </section>
+  <!-- Plunge -->
+  <section class="mb-1 grid  grid-flow-col gap-1">
+    <Button value="P" />
+    <Button value="LP" />
+    <Button value="HP" />
+  </section>
+</div>
