1309a563642efe530a745f81f3d35078b2276355
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-06-22 19:32:06 -0400

    Removed Edit btn and added ComboAddButton

---
+++ src/components/TableRow/ComboAddButton.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import ShortModal from '../Modal/ShortModal.svelte';
+  import ComboModal from '../Modal/Combo/Combo.svelte';
+
+  let dialog: HTMLDialogElement;
+
+  function toggleModal() {
+    dialog.showModal();
+  }
+</script>
+
+<button
+  class="flex h-8 w-8 items-center justify-center rounded-full bg-slate-700"
+  on:click={toggleModal}
+>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="1em"
+    viewBox="0 0 448 512"
+    class="fill-slate-300"
+  >
+    <path
+      d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"
+    />
+  </svg>
+</button>
+
+<ShortModal bind:dialog modalTitle="Talents">
+  <ComboModal />
+</ShortModal>
--- src/components/TableRow/ComboRow.svelte
+++ src/components/TableRow/ComboRow.svelte
@@ -1,18 +1,19 @@
-<script>
+<script lang="ts">
   import ComboButton from './ComboButton.svelte';
+  import ComboAddButton from './ComboAddButton.svelte';
 </script>
 
 <section class="my-1 border-b border-slate-700 pb-2">
-  <div class="flex">
+  <div class="flex items-center">
     <ComboButton />
     <ComboButton />
     <ComboButton />
+    <ComboAddButton />
   </div>
   <div class="flex justify-between">
     <div class="mt-1 flex">
       <img class="h-5 w-5 self-center" src="/images/elements/anemo.svg" alt="element" />
       <span>246,192</span>
     </div>
-    <button class="">Edit</button>
   </div>
 </section>
