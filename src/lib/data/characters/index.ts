23f8d2e5e90bfe01bd62d0ce9d2269db68fbccf3
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-06-30 21:59:36 -0400

    Added individual character + talent files w/ index

---
+++ src/lib/data/characters/index.ts
@@ -0,0 +1,133 @@
+//characters/index.ts
+
+import { default as traveleranemo } from "./traveleranemo";
+import { default as travelergeo } from "./travelergeo";
+import { default as travelerelectro } from "./travelerelectro";
+import { default as travelerdendro } from "./travelerdendro";
+import { default as albedo } from "./albedo";
+import { default as aloy } from "./aloy";
+import { default as amber } from "./amber";
+import { default as aratakiitto } from "./aratakiitto";
+import { default as barbara } from "./barbara";
+import { default as beidou } from "./beidou";
+import { default as bennett } from "./bennett";
+import { default as candace } from "./candace";
+import { default as chongyun } from "./chongyun";
+import { default as collei } from "./collei";
+import { default as cyno } from "./cyno";
+import { default as diluc } from "./diluc";
+import { default as diona } from "./diona";
+import { default as dori } from "./dori";
+import { default as eula } from "./eula";
+import { default as faruzan } from "./faruzan";
+import { default as fischl } from "./fischl";
+import { default as ganyu } from "./ganyu";
+import { default as gorou } from "./gorou";
+import { default as hutao } from "./hutao";
+import { default as jean } from "./jean";
+import { default as kaedeharakazuha } from "./kaedeharakazuha";
+import { default as kaeya } from "./kaeya";
+import { default as kamisatoayaka } from "./kamisatoayaka";
+import { default as kamisatoayato } from "./kamisatoayato";
+import { default as keqing } from "./keqing";
+import { default as klee } from "./klee";
+import { default as kujousara } from "./kujousara";
+import { default as kukishinobu } from "./kukishinobu";
+import { default as layla } from "./layla";
+import { default as lisa } from "./lisa";
+import { default as mona } from "./mona";
+import { default as nahida } from "./nahida";
+import { default as nilou } from "./nilou";
+import { default as ningguang } from "./ningguang";
+import { default as noelle } from "./noelle";
+import { default as qiqi } from "./qiqi";
+import { default as raidenshogun } from "./raidenshogun";
+import { default as razor } from "./razor";
+import { default as rosaria } from "./rosaria";
+import { default as sangonomiyakokomi } from "./sangonomiyakokomi";
+import { default as sayu } from "./sayu";
+import { default as shenhe } from "./shenhe";
+import { default as shikanoinheizou } from "./shikanoinheizou";
+import { default as sucrose } from "./sucrose";
+import { default as tartaglia } from "./tartaglia";
+import { default as thoma } from "./thoma";
+import { default as tighnari } from "./tighnari";
+import { default as venti } from "./venti";
+import { default as wanderer } from "./wanderer";
+import { default as xiangling } from "./xiangling";
+import { default as xiao } from "./xiao";
+import { default as xingqiu } from "./xingqiu";
+import { default as xinyan } from "./xinyan";
+import { default as yaemiko } from "./yaemiko";
+import { default as yanfei } from "./yanfei";
+import { default as yelan } from "./yelan";
+import { default as yoimiya } from "./yoimiya";
+import { default as yunjin } from "./yunjin";
+import { default as zhongli } from "./zhongli";
+
+export const characterData = {
+  traveleranemo,
+  travelergeo,
+  travelerelectro,
+  travelerdendro,
+  albedo,
+  aloy,
+  amber,
+  aratakiitto,
+  barbara,
+  beidou,
+  bennett,
+  candace,
+  chongyun,
+  collei,
+  cyno,
+  diluc,
+  diona,
+  dori,
+  eula,
+  faruzan,
+  fischl,
+  ganyu,
+  gorou,
+  hutao,
+  jean,
+  kaedeharakazuha,
+  kaeya,
+  kamisatoayaka,
+  kamisatoayato,
+  keqing,
+  klee,
+  kujousara,
+  kukishinobu,
+  layla,
+  lisa,
+  mona,
+  nahida,
+  nilou,
+  ningguang,
+  noelle,
+  qiqi,
+  raidenshogun,
+  razor,
+  rosaria,
+  sangonomiyakokomi,
+  sayu,
+  shenhe,
+  shikanoinheizou,
+  sucrose,
+  tartaglia,
+  thoma,
+  tighnari,
+  venti,
+  wanderer,
+  xiangling,
+  xiao,
+  xingqiu,
+  xinyan,
+  yaemiko,
+  yanfei,
+  yelan,
+  yoimiya,
+  yunjin,
+  zhongli,
+};
---
+++ src/lib/data/characters/lisa.ts
@@ -0,0 +1,173 @@
+
+import type { CharacterRecord } from '$lib/types/global';
+
+const charName: CharacterRecord = {
+  "name": "lisa",
+  "fullName": "Lisa",
+  "rating": 4,
+  "weapon": "catalyst",
+  "vision": "electro",
+  "specialized": "em",
+  "burstCost": 80,
+  "talentNames": {
+    "normal": "normalName",
+    "skill": "skillName",
+    "burst": "burstName"
+  },
+  "normal": [
+    {
+      "name": "1 - Hit DMG",
+      "tag": "N1",
+      "damageBonus": "normal",
+      "damage": [
+        {
+          "scaling": "atk",
+          "param": "param1"
+        }
+      ]
+    },
+    {
+      "name": "2 - Hit DMG",
+      "tag": "N2",
+      "damageBonus": "normal",
+      "damage": [
+        {
+          "scaling": "atk",
+          "param": "param2"
+        }
+      ]
+    },
+    {
+      "name": "3 - Hit DMG",
+      "tag": "N3",
+      "damageBonus": "normal",
+      "damage": [
+        {
+          "scaling": "atk",
+          "param": "param3"
+        }
+      ]
+    },
+    {
+      "name": "4 - Hit DMG",
+      "tag": "N4",
+      "damageBonus": "normal",
+      "damage": [
+        {
+          "scaling": "atk",
+          "param": "param4"
+        }
+      ]
+    },
+    {
+      "name": "5 - Hit DMG",
+      "tag": "N5",
+      "damageBonus": "normal",
+      "damage": [
+        {
+          "scaling": "atk",
+          "param": "param5"
+        }
+      ]
+    }
+  ],
+  "charged": [
+    {
+      "name": "Charged Attack",
+      "tag": "CA",
+      "damageBonus": "charged",
+      "damage": [
+        {
+          "scaling": "atk",
+          "param": "param6"
+        }
+      ]
+    }
+  ],
+  "plunge": [
+    {
+      "name": "Plunge DMG",
+      "tag": "PD",
+      "damageBonus": "plunge",
+      "damage": [
+        {
+          "scaling": "atk",
+          "param": "param9",
+          "icd": 0
+        }
+      ]
+    },
+    {
+      "name": "Low Plunge",
+      "tag": "PL",
+      "damageBonus": "plunge",
+      "damage": [
+        {
+          "scaling": "atk",
+          "param": "param10",
+          "icd": 0
+        }
+      ]
+    },
+    {
+      "name": "High Plunge",
+      "tag": "PH",
+      "damageBonus": "plunge",
+      "damage": [
+        {
+          "scaling": "atk",
+          "param": "param11",
+          "icd": 0
+        }
+      ]
+    }
+  ],
+  "skill": [
+    {
+      "name": "Skill DMG",
+      "tag": "EP",
+      "damageBonus": "skill",
+      "damage": [
+        {
+          "scaling": "atk",
+          "param": "param3",
+          "icd": 0
+        }
+      ]
+    }
+  ],
+  "burst": [
+    {
+      "name": "Burst DMG",
+      "tag": "Q1",
+      "multiHit": 9,
+      "damageBonus": "burst",
+      "damage": [
+        {
+          "scaling": "atk",
+          "param": "param1"
+        }
+      ]
+    }
+  ],
+  "actions": [
+    {
+      "name": "Uprising Whirlwind",
+      "url": "UI_Talent_S_PlayerWind_02",
+      "description": "description",
+      "level": 1,
+      "constellation": 2,
+      "target": "self",
+      "actionType": "passive",
+      "values": [
+        {
+          "scaling": "energy",
+          "coef": 0
+        }
+      ]
+    }
+  ],
+  "offField": []
+};
+
+export default charName;
