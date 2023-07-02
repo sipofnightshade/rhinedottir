18cc507f1a45230578dfb3faf9351fea5f463908
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-07-02 12:14:35 -0400

    Combos now reactinve and reset on character change

--- src/components/Table/Combo.svelte
+++ src/components/Table/Combo.svelte
@@ -1,20 +1,32 @@
 <script lang="ts">
   import ComboRow from '../TableRow/ComboRow.svelte';
   import type { Combo } from '$lib/types/talents';
+  import { character } from '$lib/stores/characterStore';
 
-  let aetherCombos: Combo[] = [];
+  type CharacterCombos = {
+    id: string;
+    rows: Combo[];
+  };
+
+  let combos: CharacterCombos;
+
+  $: {
+    const selected = $character.selected.name + $character.selected.vision;
+    combos = {
+      id: selected,
+      rows: []
+    };
+  }
 
   function addComboRow() {
-    aetherCombos = [...aetherCombos, { title: 'New Combo', hits: [] }];
+    combos.rows = [...combos.rows, { title: 'New Combo', hits: [] }];
   }
 </script>
 
 <div class="flex flex-col">
-  {#if aetherCombos.length > 0}
-    {#each aetherCombos as row}
-      <ComboRow {row} />
-    {/each}
-  {/if}
+  {#each combos.rows as row}
+    <ComboRow {row} />
+  {/each}
   <button
     class="my-4 flex h-16 items-center justify-center rounded-lg border-2 border-dashed border-slate-400 opacity-60 transition-opacity hover:opacity-100"
     on:click={addComboRow}
