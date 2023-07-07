0d5445fa45c70141507ea6d6012febd56478a3b5
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-07-06 22:10:55 -0400

    ✅ getCalculatedStat default case refactored from 0

--- src/lib/helpers/getCalculatedStat.ts
+++ src/lib/helpers/getCalculatedStat.ts
@@ -1,4 +1,3 @@
-// ❗❗❗❗ Make sure to add all new stats here or they won't be calculated
 export const getCalculatedStat = (
   base: { atk: number; hp: number; def: number },
   stat: string,
@@ -11,24 +10,7 @@ export const getCalculatedStat = (
       return base.atk * value;
     case 'def%':
       return base.def * value;
-    case 'hp':
-    case 'atk':
-    case 'def':
-    case 'physical':
-    case 'anemo':
-    case 'cryo':
-    case 'dendro':
-    case 'electro':
-    case 'geo':
-    case 'hydro':
-    case 'pyro':
-    case 'em':
-    case 'energy':
-    case 'critrate':
-    case 'critdmg':
-    case 'healing':
-      return value;
     default:
-      return 0;
+      return value;
   }
 };
