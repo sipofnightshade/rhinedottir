f4bc3a760e463532351ca35e5c0dbd4ffbfad9c1
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-07-16 11:45:37 -0400

    🛠 Damage rows now only show the total crit damage

--- src/components/Table/Damage.svelte
+++ src/components/Table/Damage.svelte
@@ -5,22 +5,19 @@
   // stores
   import { character } from '$lib/stores/characterStore';
   import { talents } from '$lib/stores/talentStore';
-
-  const infusion = 'physical';
 </script>
 
 <div>
   <div
-    class="mb-1.5 grid grid-cols-24 border-b border-slate-500 px-0.5 pb-1.5 text-sm font-bold uppercase"
+    class="mb-1.5 grid grid-cols-20 border-b border-slate-500 px-0.5 pb-1.5 text-sm font-bold uppercase"
   >
     <Cell align="start" col="col-span-2" value="ID" />
-    <Cell align="start" col="col-span-9" value="Talent" />
-    <Cell align="center" col="col-span-3" value="-" />
-    <Cell align="end" col="col-span-4" value="Base" />
-    <Cell align="end" col="col-span-6" value="Total" />
+    <Cell align="start" col="col-span-10" value="Talent" />
+    <Cell align="center" col="col-span-2" value="-" />
+    <Cell align="end" col="col-span-6" value="Damage" />
   </div>
 
-  <div class=" grid grid-cols-24 rounded-sm bg-slate-600 py-1 px-1.5 text-tb">
+  <div class=" grid grid-cols-20 rounded-sm bg-slate-600 py-1 px-1.5 text-tb">
     <Cell
       align="start"
       col="col-span-full"
@@ -45,7 +42,7 @@
     <TalentRow {data} />
   {/each}
 
-  <div class=" grid grid-cols-24 rounded-sm bg-slate-600 py-1 px-1.5 text-tb">
+  <div class=" grid grid-cols-20 rounded-sm bg-slate-600 py-1 px-1.5 text-tb">
     <Cell
       align="start"
       col="col-span-full"
@@ -57,7 +54,7 @@
     <TalentRow {data} />
   {/each}
   <!---------- B U R S T ---------->
-  <div class=" grid grid-cols-24 rounded-sm bg-slate-600 py-1 px-1.5 text-tb">
+  <div class=" grid grid-cols-20 rounded-sm bg-slate-600 py-1 px-1.5 text-tb">
     <Cell
       align="start"
       col="col-span-full"
--- src/components/TableRow/TalentRow.svelte
+++ src/components/TableRow/TalentRow.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
   import Cell from './Cell.svelte';
-
   import StatImage from '../Desktop/StatImage.svelte';
 
   export let data: any;
 </script>
 
-<div class="grid grid-cols-24 px-1 py-1 text-tb">
+<div class="grid grid-cols-20 px-1 py-1 text-tb">
   <Cell align="start" col="col-span-2" value={data.tag || '-'} />
   <Cell align="start" col="col-span-10 text-slate-400" value={data.name} />
   <Cell align="center" col="col-span-2">
     <StatImage stat={data.elemental} />
   </Cell>
-  <Cell align="end" col="col-span-4 text-slate-400" value={data.damage.base.toFixed(0)} />
-  <Cell align="end" col="col-span-6" value={(data.damage.base * 1.5).toFixed(0)} />
+  <!-- <Cell align="end" col="col-span-4 text-slate-400" value={nonCritDMG.toFixed(0)} /> -->
+  <Cell align="end" col="col-span-6" value={data.damage.base.toFixed(0)} />
 </div>
