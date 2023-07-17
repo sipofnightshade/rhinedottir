99cd5968e08fb776937b9c97e6c36a93b4d87224
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-07-17 17:30:24 -0400

    TalentRow now renders damage with commas

--- src/components/TableRow/TalentRow.svelte
+++ src/components/TableRow/TalentRow.svelte
@@ -3,6 +3,7 @@
   import StatImage from '../Desktop/StatImage.svelte';
 
   export let data: any;
+  $: damage = Math.floor(data.damage.base).toLocaleString();
 </script>
 
 <div class="grid grid-cols-20 px-1 py-1 text-tb">
@@ -12,5 +13,5 @@
     <StatImage stat={data.elemental} />
   </Cell>
   <!-- <Cell align="end" col="col-span-4 text-slate-400" value={nonCritDMG.toFixed(0)} /> -->
-  <Cell align="end" col="col-span-6" value={data.damage.base.toFixed(0)} />
+  <Cell align="end" col="col-span-6" value={damage} />
 </div>
