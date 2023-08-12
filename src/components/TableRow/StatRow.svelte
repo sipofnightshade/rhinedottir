6b34ca739a3e0474b2e9ad0aa82595557e4a12c5
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-08-12 18:00:31 -0400

    Update: added logic to hide empty stats; 🛠

--- src/components/TableRow/StatRow.svelte
+++ src/components/TableRow/StatRow.svelte
@@ -18,10 +18,14 @@
       : isPcnt
       ? (total * 100).toFixed(0) + '%' // Multiply by 100 and remove decimals
       : numFormat.format(+total.toFixed(0));
+
+  $: showRow = total > 0;
 </script>
 
+<!-- {#if showRow} -->
 <div class="grid grid-cols-20 px-0.5 py-1 text-tb">
   <Cell align="start" col="col-span-12" value={label} />
   <Cell align="end" col="col-span-4" value={displayBase} />
   <Cell align="end" col="col-span-4" value={displayTotal} />
 </div>
+<!-- {/if} -->
