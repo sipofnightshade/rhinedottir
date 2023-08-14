d61faff5829e6123f2eadaf41dd5ecb0728af47a
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-08-14 19:03:47 -0400

    New: created function to calculate action coefs✅

---
+++ src/lib/calculators/calcCoefficient.ts
@@ -0,0 +1,30 @@
+import type { All_Stats } from '$lib/stores/actionStore';
+import type { CoefSource } from '$lib/types/actions';
+
+export function calcCoeficient(
+  coef: number,
+  $stats: Record<All_Stats, number>,
+  source?: CoefSource
+): number {
+  if (source === undefined) {
+    return coef;
+  }
+
+  const [statType, threshold, max] = source;
+
+  if ($stats && statType in $stats) {
+    const statValue = $stats[statType];
+
+    if (statValue >= threshold) {
+      const multipliedValue = coef * statValue - threshold;
+
+      if (max !== undefined && multipliedValue > max) {
+        return max;
+      }
+
+      return multipliedValue;
+    }
+  }
+
+  return 0;
+}
