9ce9a6c845b87abb465c6133b181d70a6c12d890
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-08-28 18:36:49 -0400

    Update: Baizhu file finished ✅

--- src/lib/data/characters/baizhu.ts
+++ src/lib/data/characters/baizhu.ts
@@ -1,194 +1,328 @@
-
 import type { CharacterRecord } from '$lib/types/global';
 
 const charName: CharacterRecord = {
-  "name": "baizhu",
-  "fullName": "Baizhu",
-  "rating": 5,
-  "weapon": "catalyst",
-  "region": "mondstat",
-  "vision": "dendro",
-  "specialized": "hp%",
-  "c3": "skill",
-  "c5": "burst",
-  "burstCost": 80,
-  "talentNames": {
-    "normal": "normalName",
-    "skill": "skillName",
-    "burst": "burstName"
+  name: 'baizhu',
+  fullName: 'Baizhu',
+  rating: 5,
+  weapon: 'catalyst',
+  region: 'liyue',
+  vision: 'dendro',
+  specialized: 'hp%',
+  c3: 'burst',
+  c5: 'skill',
+  burstCost: 80,
+  talentNames: {
+    normal: 'The Classics of Acupuncture',
+    skill: 'Universal Diagnosis',
+    burst: 'Holistic Revivification'
   },
-  "normal": [
+  normal: [
     {
-      "name": "1 - Hit DMG",
-      "tag": "N1",
-      "damageBonus": "normal",
-      "damage": [
+      name: '1 - Hit DMG',
+      tag: 'N1',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param1"
+            scaling: 'atk',
+            param: 'param1'
           }
         ]
       ]
     },
     {
-      "name": "2 - Hit DMG",
-      "tag": "N2",
-      "damageBonus": "normal",
-      "damage": [
+      name: '2 - Hit DMG',
+      tag: 'N2',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param2"
+            scaling: 'atk',
+            param: 'param2'
           }
         ]
       ]
     },
     {
-      "name": "3 - Hit DMG",
-      "tag": "N3",
-      "damageBonus": "normal",
-      "damage": [
+      name: '3 - Hit DMG',
+      tag: 'N3',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param3"
+            scaling: 'atk',
+            param: 'param3'
+          }
+        ],
+        [
+          {
+            scaling: 'atk',
+            param: 'param3'
           }
         ]
       ]
     },
     {
-      "name": "4 - Hit DMG",
-      "tag": "N4",
-      "damageBonus": "normal",
-      "damage": [
+      name: '4 - Hit DMG',
+      tag: 'N4',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param4"
-          }
-        ]
-      ]
-    },
-    {
-      "name": "5 - Hit DMG",
-      "tag": "N5",
-      "damageBonus": "normal",
-      "damage": [
-        [
-          {
-            "scaling": "atk",
-            "param": "param5"
+            scaling: 'atk',
+            param: 'param4'
           }
         ]
       ]
     }
   ],
-  "charged": [
+  charged: [
     {
-      "name": "Charged Attack",
-      "tag": "CA",
-      "damageBonus": "charged",
-      "damage": [
+      name: 'Charged Attack',
+      tag: 'CA',
+      damageBonus: 'charged',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param6"
+            scaling: 'atk',
+            param: 'param5'
           }
         ]
       ]
     }
   ],
-  "plunge": [
+  plunge: [
     {
-      "name": "Plunge DMG",
-      "tag": "PD",
-      "damageBonus": "plunge",
-      "damage": [
+      name: 'Plunge DMG',
+      tag: 'PD',
+      damageBonus: 'plunge',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param9"
+            scaling: 'atk',
+            param: 'param7'
           }
         ]
       ]
     },
     {
-      "name": "Low Plunge",
-      "tag": "PL",
-      "damageBonus": "plunge",
-      "damage": [
+      name: 'Low Plunge',
+      tag: 'PL',
+      damageBonus: 'plunge',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param10"
+            scaling: 'atk',
+            param: 'param8'
           }
         ]
       ]
     },
     {
-      "name": "High Plunge",
-      "tag": "PH",
-      "damageBonus": "plunge",
-      "damage": [
+      name: 'High Plunge',
+      tag: 'PH',
+      damageBonus: 'plunge',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param11"
+            scaling: 'atk',
+            param: 'param9'
           }
         ]
       ]
     }
   ],
-  "skill": [
+  skill: [
     {
-      "name": "Skill DMG",
-      "tag": "EP",
-      "damageBonus": "skill",
-      "damage": [
+      name: 'Skill DMG',
+      tag: 'E',
+      damageBonus: 'skill',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param3"
+            scaling: 'atk',
+            param: 'param1'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Healing',
+      damageBonus: 'skill',
+      isHealing: true,
+      damage: [
+        [
+          {
+            scaling: 'hp',
+            param: 'param2'
+          },
+          {
+            scaling: 'flatValue',
+            param: 'param3'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Incisive Discernment',
+      tag: 'X2',
+      damageBonus: 'skill',
+      constellation: 2,
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param1',
+            coef: 2.5
           }
         ]
       ]
     }
   ],
-  "burst": [
+  burst: [
     {
-      "name": "Burst DMG",
-      "tag": "Q1",
-      "multiHit": 9,
-      "damageBonus": "burst",
-      "damage": [
+      name: 'Spiritvein DMG',
+      tag: 'Q',
+      damageBonus: 'burst',
+      hasOwnBonusFlatDMG: true,
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param1"
+            scaling: 'atk',
+            param: 'param7'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Seamless Shield DMG Absorption',
+      damageBonus: 'skill',
+      isShield: true,
+      damage: [
+        [
+          {
+            scaling: 'hp',
+            param: 'param1'
+          },
+          {
+            scaling: 'flatValue',
+            param: 'param2'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Seamless Shield Healing',
+      damageBonus: 'skill',
+      isHealing: true,
+      damage: [
+        [
+          {
+            scaling: 'hp',
+            param: 'param5'
+          },
+          {
+            scaling: 'flatValue',
+            param: 'param6'
           }
         ]
       ]
     }
   ],
-  "actions": [
+  actions: [
     {
-      "name": "Uprising Whirlwind",
-      "url": "UI_Talent_S_PlayerWind_02",
-      "description": "description",
-      "level": 1,
-      "constellation": 2,
-      "target": "self",
-      "actionType": "passive",
-      "values": [
+      name: 'Five Fortunes Forever',
+      url: 'UI_Talent_S_Baizhuer_05',
+      description:
+        'Baizhu gains different effects according to the current HP of your current active character: \n·When their HP is less than 50%, Baizhu gains 20% Healing Bonus. \n·When their HP is equal to or more than 50%, Baizhu gains 25% Dendro DMG Bonus.',
+      level: 2,
+      target: 'self',
+      actionType: 'select',
+      values: [
         {
-          "scaling": "energy",
-          "coef": 0
+          scaling: 'healing',
+          coef: 0.2
+        },
+        {
+          scaling: 'dendro',
+          coef: 0.25
+        }
+      ]
+    },
+    {
+      name: 'All Things Are of the Earth',
+      url: 'UI_Talent_S_Baizhuer_06',
+      description:
+        'Characters who are healed by Seamless Shields will gain the Year of Verdant Favor effect: Each 1,000 Max HP that Baizhu possesses that does not exceed 50,000 will increase the Burning, Bloom, Hyperbloom, and Burgeon reaction DMG dealt by these characters by 2%, and will increase the DMG Bonus provided by Aggravate and Spread by 0.8%. This effect lasts 6s.',
+      level: 8,
+      target: 'party',
+      actionType: 'toggle',
+      values: [
+        {
+          scaling: 'burning',
+          coef: 0.02,
+          source: ['hp', 0, 50000, 1000]
+        },
+        {
+          scaling: 'bloom',
+          coef: 0.02,
+          source: ['hp', 0, 50000, 1000]
+        },
+        {
+          scaling: 'hyperbloom',
+          coef: 0.02,
+          source: ['hp', 0, 50000, 1000]
+        },
+        {
+          scaling: 'burgeon',
+          coef: 0.02,
+          source: ['hp', 0, 50000, 1000]
+        },
+        {
+          scaling: 'spread',
+          coef: 0.08,
+          source: ['hp', 0, 50000, 1000]
+        },
+        {
+          scaling: 'aggravate',
+          coef: 0.08,
+          source: ['hp', 0, 50000, 1000]
+        }
+      ]
+    },
+    {
+      name: 'Ancient Art of Perception',
+      url: 'UI_Talent_S_Baizhuer_03',
+      description:
+        "For 15s after Holistic Revivification is used, Baizhu will increase all nearby party members' Elemental Mastery by 80.",
+      constellation: 4,
+      target: 'nearby',
+      actionType: 'toggle',
+      values: [
+        {
+          scaling: 'em',
+          coef: 80
+        }
+      ]
+    },
+    {
+      name: 'Elimination of Malicious Qi',
+      url: 'UI_Talent_S_Baizhuer_04',
+      description:
+        "For 15s after Holistic Revivification is used, Baizhu will increase all nearby party members' Elemental Mastery by 80.",
+      constellation: 6,
+      target: 'nearby',
+      actionType: 'toggle',
+      values: [
+        {
+          scaling: 'cBonusFlatDMG',
+          coef: 0.08,
+          source: ['hp', 0]
         }
       ]
     }
   ],
-  "offField": []
+  offField: []
 };
 
 export default charName;
