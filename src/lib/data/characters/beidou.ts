b1ed5622b424684997305cc8819dd2b2b66a421f
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-08-28 23:37:42 -0400

    Update: Beidou file finished ✅

--- src/lib/data/characters/beidou.ts
+++ src/lib/data/characters/beidou.ts
@@ -1,194 +1,330 @@
-
 import type { CharacterRecord } from '$lib/types/global';
 
 const charName: CharacterRecord = {
-  "name": "beidou",
-  "fullName": "Beidou",
-  "rating": 4,
-  "weapon": "claymore",
-  "region": "mondstat",
-  "vision": "electro",
-  "specialized": "electro",
-  "c3": "skill",
-  "c5": "burst",
-  "burstCost": 80,
-  "talentNames": {
-    "normal": "normalName",
-    "skill": "skillName",
-    "burst": "burstName"
+  name: 'beidou',
+  fullName: 'Beidou',
+  rating: 4,
+  weapon: 'claymore',
+  region: 'mondstat',
+  vision: 'electro',
+  specialized: 'electro',
+  c3: 'skill',
+  c5: 'burst',
+  burstCost: 80,
+  talentNames: {
+    normal: 'Oceanborne',
+    skill: 'Tidecaller',
+    burst: 'Stormbreaker'
   },
-  "normal": [
+  normal: [
     {
-      "name": "1 - Hit DMG",
-      "tag": "N1",
-      "damageBonus": "normal",
-      "damage": [
+      name: '1 - Hit DMG',
+      tag: 'N1',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param1"
+            scaling: 'atk',
+            param: 'param1'
           }
         ]
       ]
     },
     {
-      "name": "2 - Hit DMG",
-      "tag": "N2",
-      "damageBonus": "normal",
-      "damage": [
+      name: '2 - Hit DMG',
+      tag: 'N2',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param2"
+            scaling: 'atk',
+            param: 'param2'
           }
         ]
       ]
     },
     {
-      "name": "3 - Hit DMG",
-      "tag": "N3",
-      "damageBonus": "normal",
-      "damage": [
+      name: '3 - Hit DMG',
+      tag: 'N3',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param3"
+            scaling: 'atk',
+            param: 'param3'
           }
         ]
       ]
     },
     {
-      "name": "4 - Hit DMG",
-      "tag": "N4",
-      "damageBonus": "normal",
-      "damage": [
+      name: '4 - Hit DMG',
+      tag: 'N4',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param4"
+            scaling: 'atk',
+            param: 'param4'
           }
         ]
       ]
     },
     {
-      "name": "5 - Hit DMG",
-      "tag": "N5",
-      "damageBonus": "normal",
-      "damage": [
+      name: '5 - Hit DMG',
+      tag: 'N5',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param5"
+            scaling: 'atk',
+            param: 'param5'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Stunning Revenge',
+      tag: 'X4',
+      damageBonus: 'normal',
+      hasOwnDMGType: 'electro',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param1',
+            coef: 0.2
           }
         ]
       ]
     }
   ],
-  "charged": [
+  charged: [
     {
-      "name": "Charged Attack",
-      "tag": "CA",
-      "damageBonus": "charged",
-      "damage": [
+      name: 'Charged Attack Cyclic DMG',
+      tag: 'CA',
+      damageBonus: 'charged',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param6"
-          }
-        ]
-      ]
-    }
-  ],
-  "plunge": [
-    {
-      "name": "Plunge DMG",
-      "tag": "PD",
-      "damageBonus": "plunge",
-      "damage": [
-        [
-          {
-            "scaling": "atk",
-            "param": "param9"
+            scaling: 'atk',
+            param: 'param6'
           }
         ]
       ]
     },
     {
-      "name": "Low Plunge",
-      "tag": "PL",
-      "damageBonus": "plunge",
-      "damage": [
+      name: 'Charged Attack Final DMG',
+      tag: 'CF',
+      damageBonus: 'charged',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param10"
+            scaling: 'atk',
+            param: 'param7'
+          }
+        ]
+      ]
+    }
+  ],
+  plunge: [
+    {
+      name: 'Plunge DMG',
+      tag: 'PD',
+      damageBonus: 'plunge',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param10'
           }
         ]
       ]
     },
     {
-      "name": "High Plunge",
-      "tag": "PH",
-      "damageBonus": "plunge",
-      "damage": [
+      name: 'Low Plunge',
+      tag: 'PL',
+      damageBonus: 'plunge',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param11"
+            scaling: 'atk',
+            param: 'param11'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'High Plunge',
+      tag: 'PH',
+      damageBonus: 'plunge',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param12'
           }
         ]
       ]
     }
   ],
-  "skill": [
+  skill: [
     {
-      "name": "Skill DMG",
-      "tag": "EP",
-      "damageBonus": "skill",
-      "damage": [
+      name: 'Shield DMG Absorption',
+      damageBonus: 'skill',
+      isShield: true,
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param3"
+            scaling: 'hp',
+            param: 'param1'
+          },
+          {
+            scaling: 'flatValue',
+            param: 'param2'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Base DMG',
+      tag: 'E0',
+      damageBonus: 'skill',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param3'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'DMG Bonus on Hit Taken',
+      damageBonus: 'skill',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param4'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Total DMG  1 Hit Taken',
+      tag: 'E1',
+      damageBonus: 'skill',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param3'
+          },
+          {
+            scaling: 'atk',
+            param: 'param4'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'DMG With 2+ Hits Taken',
+      tag: 'E2',
+      damageBonus: 'skill',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param3'
+          },
+          {
+            scaling: 'atk',
+            param: 'param4'
+          },
+          {
+            scaling: 'atk',
+            param: 'param4'
           }
         ]
       ]
     }
   ],
-  "burst": [
+  burst: [
     {
-      "name": "Burst DMG",
-      "tag": "Q1",
-      "multiHit": 9,
-      "damageBonus": "burst",
-      "damage": [
+      name: 'Burst DMG',
+      tag: 'Q',
+      damageBonus: 'burst',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param1"
+            scaling: 'atk',
+            param: 'param1'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Lightning DMG',
+      tag: 'QC',
+      damageBonus: 'burst',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param2'
+          }
+        ]
+      ]
+    },
+    {
+      name: "Sea Beast's Scourge",
+      damageBonus: 'burst',
+      isShield: true,
+      constellation: 1,
+      damage: [
+        [
+          {
+            scaling: 'hp',
+            param: 'param1',
+            coef: 0.16
           }
         ]
       ]
     }
   ],
-  "actions": [
+  actions: [
     {
-      "name": "Uprising Whirlwind",
-      "url": "UI_Talent_S_PlayerWind_02",
-      "description": "description",
-      "level": 1,
-      "constellation": 2,
-      "target": "self",
-      "actionType": "passive",
-      "values": [
+      name: 'Lightning Storm',
+      url: 'UI_Talent_S_Beidou_06',
+      description: 'description',
+      level: 8,
+      target: 'self',
+      actionType: 'toggle',
+      values: [
         {
-          "scaling": "energy",
-          "coef": 0
+          scaling: 'normal',
+          coef: 0.15
+        },
+        {
+          scaling: 'charged',
+          coef: 0.15
+        }
+      ]
+    },
+    {
+      name: 'Bane of Evil',
+      url: 'UI_Talent_S_Beidou_04',
+      description: 'description',
+      target: 'enemy',
+      actionType: 'toggle',
+      values: [
+        {
+          scaling: 'electroRes',
+          coef: 0.15
         }
       ]
     }
-  ],
-  "offField": []
+  ]
 };
 
 export default charName;
