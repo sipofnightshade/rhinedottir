ce293b2095c32622969f32d395bed1b3eb6b4227
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-09-06 08:58:34 -0400

    Update: KukiShinobu finished ✅

--- src/lib/data/characters/kukishinobu.ts
+++ src/lib/data/characters/kukishinobu.ts
@@ -1,194 +1,294 @@
-
 import type { CharacterRecord } from '$lib/types/global';
 
 const charName: CharacterRecord = {
-  "name": "kukishinobu",
-  "fullName": "Kuki Shinobu",
-  "rating": 4,
-  "weapon": "sword",
-  "region": "mondstat",
-  "vision": "electro",
-  "specialized": "hp%",
-  "c3": "skill",
-  "c5": "burst",
-  "burstCost": 80,
-  "talentNames": {
-    "normal": "normalName",
-    "skill": "skillName",
-    "burst": "burstName"
+  name: 'kukishinobu',
+  fullName: 'Kuki Shinobu',
+  rating: 4,
+  weapon: 'sword',
+  region: 'inazuma',
+  vision: 'electro',
+  specialized: 'hp%',
+  c3: 'skill',
+  c5: 'burst',
+  burstCost: 60,
+  talentNames: {
+    normal: "Shinobu's Shadowsword",
+    skill: 'sanctifyingRing',
+    burst: 'Gyoei Narukami Kariyama Rite'
   },
-  "normal": [
+  normal: [
     {
-      "name": "1 - Hit DMG",
-      "tag": "N1",
-      "damageBonus": "normal",
-      "damage": [
+      name: '1 - Hit DMG',
+      tag: 'N1',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param1"
+            scaling: 'atk',
+            param: 'param1'
           }
         ]
       ]
     },
     {
-      "name": "2 - Hit DMG",
-      "tag": "N2",
-      "damageBonus": "normal",
-      "damage": [
+      name: '2 - Hit DMG',
+      tag: 'N2',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param2"
+            scaling: 'atk',
+            param: 'param2'
           }
         ]
       ]
     },
     {
-      "name": "3 - Hit DMG",
-      "tag": "N3",
-      "damageBonus": "normal",
-      "damage": [
+      name: '3 - Hit DMG',
+      tag: 'N3',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param3"
+            scaling: 'atk',
+            param: 'param3'
           }
         ]
       ]
     },
     {
-      "name": "4 - Hit DMG",
-      "tag": "N4",
-      "damageBonus": "normal",
-      "damage": [
+      name: '4 - Hit DMG',
+      tag: 'N4',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param4"
-          }
-        ]
-      ]
-    },
-    {
-      "name": "5 - Hit DMG",
-      "tag": "N5",
-      "damageBonus": "normal",
-      "damage": [
-        [
-          {
-            "scaling": "atk",
-            "param": "param5"
+            scaling: 'atk',
+            param: 'param4'
           }
         ]
       ]
     }
   ],
-  "charged": [
+  charged: [
     {
-      "name": "Charged Attack",
-      "tag": "CA",
-      "damageBonus": "charged",
-      "damage": [
+      name: 'Charged Attack',
+      tag: 'CA',
+      damageBonus: 'charged',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param6"
+            scaling: 'atk',
+            param: 'param5'
+          }
+        ],
+        [
+          {
+            scaling: 'atk',
+            param: 'param6'
           }
         ]
       ]
     }
   ],
-  "plunge": [
+  plunge: [
     {
-      "name": "Plunge DMG",
-      "tag": "PD",
-      "damageBonus": "plunge",
-      "damage": [
+      name: 'Plunge DMG',
+      tag: 'PD',
+      damageBonus: 'plunge',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param9"
+            scaling: 'atk',
+            param: 'param8'
           }
         ]
       ]
     },
     {
-      "name": "Low Plunge",
-      "tag": "PL",
-      "damageBonus": "plunge",
-      "damage": [
+      name: 'Low Plunge',
+      tag: 'PL',
+      damageBonus: 'plunge',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param10"
+            scaling: 'atk',
+            param: 'param9'
           }
         ]
       ]
     },
     {
-      "name": "High Plunge",
-      "tag": "PH",
-      "damageBonus": "plunge",
-      "damage": [
+      name: 'High Plunge',
+      tag: 'PH',
+      damageBonus: 'plunge',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param11"
+            scaling: 'atk',
+            param: 'param10'
           }
         ]
       ]
     }
   ],
-  "skill": [
+  skill: [
     {
-      "name": "Skill DMG",
-      "tag": "EP",
-      "damageBonus": "skill",
-      "damage": [
+      name: 'Skill DMG',
+      tag: 'E',
+      hasOwnBonusFlatDMG: 'sanctifyingRing',
+      damageBonus: 'skill',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param3"
+            scaling: 'atk',
+            param: 'param1'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Grass Ring of Sanctification DMG',
+      tag: 'EC',
+      hasOwnBonusFlatDMG: 'sanctifyingRing',
+      damageBonus: 'skill',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param4'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Grass Ring of Sanctification Healing',
+      isHealing: true,
+      damageBonus: 'skill',
+      damage: [
+        [
+          {
+            scaling: 'hp',
+            param: 'param2'
+          },
+          {
+            scaling: 'flatValue',
+            param: 'param3'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Thundergrass Mark DMG',
+      tag: 'X4',
+      constellation: 4,
+      damageBonus: 'skill',
+      damage: [
+        [
+          {
+            scaling: 'hp',
+            param: 'param4',
+            coef: 0.097
           }
         ]
       ]
     }
   ],
-  "burst": [
+  burst: [
     {
-      "name": "Burst DMG",
-      "tag": "Q1",
-      "multiHit": 9,
-      "damageBonus": "burst",
-      "damage": [
+      name: 'Single Instance DMG',
+      damageBonus: 'burst',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param1"
+            scaling: 'hp',
+            param: 'param1'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Total DMG',
+      tag: 'Q1',
+      damageBonus: 'burst',
+      damage: [
+        [
+          {
+            scaling: 'hp',
+            param: 'param2'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Total Low HP DMG',
+      tag: 'Q2',
+      damageBonus: 'burst',
+      damage: [
+        [
+          {
+            scaling: 'hp',
+            param: 'param3'
           }
         ]
       ]
     }
   ],
-  "actions": [
+  actions: [
     {
-      "name": "Uprising Whirlwind",
-      "url": "UI_Talent_S_PlayerWind_02",
-      "description": "description",
-      "level": 1,
-      "constellation": 2,
-      "target": "self",
-      "actionType": "passive",
-      "values": [
+      name: 'Breaking Free',
+      url: 'UI_Talent_S_Shinobu_05',
+      description:
+        "When Shinobu's HP is not higher than 50%, her Healing Bonus is increased by 15%.",
+      level: 2,
+      target: 'self',
+      actionType: 'toggle',
+      values: [
         {
-          "scaling": "energy",
-          "coef": 0
+          scaling: 'healing',
+          coef: 0.15
+        }
+      ]
+    },
+    {
+      name: "Heart's Repose",
+      url: 'UI_Talent_S_Shinobu_06',
+      description:
+        "**Sanctifying Ring**'s abilities will be boosted based on Shinobu's Elemental Mastery:\n·Healing amount will be increased by 75% of Elemental Mastery.\n·DMG dealt is increased by 25% of Elemental Mastery.",
+      level: 8,
+      target: 'self',
+      actionType: 'passive',
+      sourceStats: ['em'],
+      values: [
+        {
+          scaling: 'flatHealing',
+          coef: 0.75,
+          source: ['em', 0]
+        },
+        {
+          scaling: 'sanctifyingRing',
+          coef: 0.25,
+          source: ['em', 0]
+        }
+      ]
+    },
+    {
+      name: 'To Ward Weakness',
+      url: 'UI_Talent_S_Shinobu_04',
+      description:
+        "When Shinobu's HP drops below 25%, she will gain 150 Elemental Mastery for 15s",
+      constellation: 6,
+      target: 'self',
+      actionType: 'toggle',
+      values: [
+        {
+          scaling: 'em',
+          coef: 150
         }
       ]
     }
   ],
-  "offField": []
+  offField: []
 };
 
 export default charName;
