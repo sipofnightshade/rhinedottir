fae789451a87c0fe3cabd56d5b0312f04ae742f6
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-09-07 15:41:33 -0400

    Update: RaidenShogun finished ✅

--- src/lib/data/characters/raidenshogun.ts
+++ src/lib/data/characters/raidenshogun.ts
@@ -1,194 +1,461 @@
-
 import type { CharacterRecord } from '$lib/types/global';
 
 const charName: CharacterRecord = {
-  "name": "raidenshogun",
-  "fullName": "Raiden Shogun",
-  "rating": 5,
-  "weapon": "polearm",
-  "region": "mondstat",
-  "vision": "electro",
-  "specialized": "energy",
-  "c3": "skill",
-  "c5": "burst",
-  "burstCost": 80,
-  "talentNames": {
-    "normal": "normalName",
-    "skill": "skillName",
-    "burst": "burstName"
+  name: 'raidenshogun',
+  fullName: 'Raiden Shogun',
+  rating: 5,
+  weapon: 'polearm',
+  region: 'inazuma',
+  vision: 'electro',
+  specialized: 'energy',
+  c3: 'burst',
+  c5: 'skill',
+  burstCost: 90,
+  talentNames: {
+    normal: 'Origin',
+    skill: 'Transcendence: Baleful Omen',
+    burst: 'Secret Art: Musou Shinsetsu'
   },
-  "normal": [
+  normal: [
     {
-      "name": "1 - Hit DMG",
-      "tag": "N1",
-      "damageBonus": "normal",
-      "damage": [
+      name: '1 - Hit DMG',
+      tag: 'N1',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param1"
+            scaling: 'atk',
+            param: 'param1'
           }
         ]
       ]
     },
     {
-      "name": "2 - Hit DMG",
-      "tag": "N2",
-      "damageBonus": "normal",
-      "damage": [
+      name: '2 - Hit DMG',
+      tag: 'N2',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param2"
+            scaling: 'atk',
+            param: 'param2'
           }
         ]
       ]
     },
     {
-      "name": "3 - Hit DMG",
-      "tag": "N3",
-      "damageBonus": "normal",
-      "damage": [
+      name: '3 - Hit DMG',
+      tag: 'N3',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param3"
+            scaling: 'atk',
+            param: 'param3'
           }
         ]
       ]
     },
     {
-      "name": "4 - Hit DMG",
-      "tag": "N4",
-      "damageBonus": "normal",
-      "damage": [
+      name: '4 - Hit DMG',
+      tag: 'N4',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param4"
+            scaling: 'atk',
+            param: 'param4'
+          }
+        ],
+        [
+          {
+            scaling: 'atk',
+            param: 'param5'
           }
         ]
       ]
     },
     {
-      "name": "5 - Hit DMG",
-      "tag": "N5",
-      "damageBonus": "normal",
-      "damage": [
+      name: '5 - Hit DMG',
+      tag: 'N5',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param5"
+            scaling: 'atk',
+            param: 'param6'
           }
         ]
       ]
     }
   ],
-  "charged": [
+  charged: [
     {
-      "name": "Charged Attack",
-      "tag": "CA",
-      "damageBonus": "charged",
-      "damage": [
+      name: 'Charged Attack',
+      tag: 'CA',
+      damageBonus: 'charged',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param6"
+            scaling: 'atk',
+            param: 'param7'
           }
         ]
       ]
     }
   ],
-  "plunge": [
+  plunge: [
     {
-      "name": "Plunge DMG",
-      "tag": "PD",
-      "damageBonus": "plunge",
-      "damage": [
+      name: 'Plunge DMG',
+      tag: 'PD',
+      damageBonus: 'plunge',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param9"
+            scaling: 'atk',
+            param: 'param9'
           }
         ]
       ]
     },
     {
-      "name": "Low Plunge",
-      "tag": "PL",
-      "damageBonus": "plunge",
-      "damage": [
+      name: 'Low Plunge',
+      tag: 'PL',
+      damageBonus: 'plunge',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param10"
+            scaling: 'atk',
+            param: 'param10'
           }
         ]
       ]
     },
     {
-      "name": "High Plunge",
-      "tag": "PH",
-      "damageBonus": "plunge",
-      "damage": [
+      name: 'High Plunge',
+      tag: 'PH',
+      damageBonus: 'plunge',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param11"
+            scaling: 'atk',
+            param: 'param11'
           }
         ]
       ]
     }
   ],
-  "skill": [
+  skill: [
     {
-      "name": "Skill DMG",
-      "tag": "EP",
-      "damageBonus": "skill",
-      "damage": [
+      name: 'Skill DMG',
+      tag: 'E',
+      damageBonus: 'skill',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param3"
+            scaling: 'atk',
+            param: 'param1'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Coordinated ATK DMG',
+      tag: 'EC',
+      damageBonus: 'skill',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param2'
           }
         ]
       ]
     }
   ],
-  "burst": [
+  burst: [
     {
-      "name": "Burst DMG",
-      "tag": "Q1",
-      "multiHit": 9,
-      "damageBonus": "burst",
-      "damage": [
+      name: 'Musou no Hitotachi DMG',
+      tag: 'Q',
+      damageBonus: 'burst',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param1"
+            scaling: 'atk',
+            param: 'param1'
+          }
+        ]
+      ]
+    },
+    {
+      name: '1-Hit DMG',
+      tag: 'Q1',
+      damageBonus: 'burst',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param5'
+          }
+        ]
+      ]
+    },
+    {
+      name: '2-Hit DMG',
+      tag: 'Q2',
+      damageBonus: 'burst',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param6'
+          }
+        ]
+      ]
+    },
+    {
+      name: '3-Hit DMG',
+      tag: 'Q3',
+      damageBonus: 'burst',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param7'
+          }
+        ]
+      ]
+    },
+    {
+      name: '4-Hit DMG',
+      tag: 'Q4',
+      damageBonus: 'burst',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param8'
+          }
+        ],
+        [
+          {
+            scaling: 'atk',
+            param: 'param9'
+          }
+        ]
+      ]
+    },
+    {
+      name: '5-Hit DMG',
+      tag: 'Q5',
+      damageBonus: 'burst',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param10'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Charged Attack DMG',
+      tag: 'QC',
+      damageBonus: 'burst',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param11'
+          }
+        ],
+        [
+          {
+            scaling: 'atk',
+            param: 'param12'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Plunge DMG',
+      damageBonus: 'burst',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param14'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Low Plunge DMG',
+      damageBonus: 'burst',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param15'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'High Plunge DMG',
+      damageBonus: 'burst',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param16'
           }
         ]
       ]
     }
   ],
-  "actions": [
+  actions: [
     {
-      "name": "Uprising Whirlwind",
-      "url": "UI_Talent_S_PlayerWind_02",
-      "description": "description",
-      "level": 1,
-      "constellation": 2,
-      "target": "self",
-      "actionType": "passive",
-      "values": [
+      name: 'Eye of Stormy Judgment',
+      url: 'Skill_S_Shougun_01',
+      description:
+        "Characters who gain the Eye of Stormy Judgment will have their Elemental Burst DMG increased based on the Energy Cost of the Elemental Burst during the Eye's duration\n**MODIFIED**\nUses the average party burst costs for calculations",
+      target: 'party',
+      actionType: 'toggle',
+      hasLevels: 'skill',
+      sourceStats: ['partyAvgBurstCost'],
+      values: [
         {
-          "scaling": "energy",
-          "coef": 0
+          scaling: 'burst',
+          coef: 'param4',
+          source: ['partyAvgBurstCost', 0]
+        }
+      ]
+    },
+    {
+      name: 'Chakra Desiderata',
+      url: 'Skill_E_Shougun_01',
+      description:
+        'When nearby party members (excluding the Raiden Shogun herself) use their Elemental Bursts, the Raiden Shogun will build up Resolve stacks based on the Energy Cost of these Elemental Bursts. The maximum number of Resolve stacks is 60.',
+      target: 'self',
+      actionType: 'stack',
+      hasLevels: 'burst',
+      values: [
+        {
+          scaling: 'burst',
+          coef: [
+            'param2',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3',
+            'param3'
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Enlightened One',
+      url: 'UI_Talent_S_Shougun_06',
+      description:
+        'Each 1% above 100% Energy Recharge that the Raiden Shogun possesses grants her:\n·0.6% greater Energy restoration from **Musou Isshin**\n·0.4% Electro DMG Bonus.',
+      level: 8,
+      target: 'self',
+      actionType: 'passive',
+      sourceStats: ['energy'],
+      values: [
+        {
+          scaling: 'electro',
+          coef: 0.4,
+          source: ['energy', 1]
+        }
+      ]
+    },
+    {
+      name: 'Steelbreaker',
+      url: 'UI_Talent_S_Shougun_03',
+      description:
+        "While using Musou no Hitotachi and in the Musou Isshin state applied by **Secret Art: Musou Shinsetsu**, the Raiden Shogun's attacks ignore 60% of opponents' DEF.",
+      constellation: 2,
+      target: 'self',
+      actionType: 'toggle',
+      values: [
+        {
+          scaling: 'burstDefIgnore',
+          coef: 0.6
+        }
+      ]
+    },
+    {
+      name: 'Pledge of Propriety',
+      url: 'UI_Talent_S_Shougun_02',
+      description:
+        'When the **Musou Isshin** state applied by **Secret Art: Musou Shinsetsu** expires, all nearby party members (excluding the Raiden Shogun) gain 30% bonus ATK for 10s.',
+      constellation: 4,
+      target: 'nearby',
+      actionType: 'toggle',
+      values: [
+        {
+          scaling: 'atk%',
+          coef: 0.3
         }
       ]
     }
-  ],
-  "offField": []
+  ]
 };
 
 export default charName;
