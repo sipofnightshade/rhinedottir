10efc83573efef79af7eb38a506c2c5dd6094d9b
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-09-07 21:27:14 -0400

    Update: Noelle finished ✅

--- src/lib/data/characters/noelle.ts
+++ src/lib/data/characters/noelle.ts
@@ -1,194 +1,302 @@
-
 import type { CharacterRecord } from '$lib/types/global';
 
 const charName: CharacterRecord = {
-  "name": "noelle",
-  "fullName": "Noelle",
-  "rating": 4,
-  "weapon": "claymore",
-  "region": "mondstat",
-  "vision": "geo",
-  "specialized": "def%",
-  "c3": "skill",
-  "c5": "burst",
-  "burstCost": 80,
-  "talentNames": {
-    "normal": "normalName",
-    "skill": "skillName",
-    "burst": "burstName"
+  name: 'noelle',
+  fullName: 'Noelle',
+  rating: 4,
+  weapon: 'claymore',
+  region: 'mondstat',
+  vision: 'geo',
+  specialized: 'def%',
+  c3: 'skill',
+  c5: 'burst',
+  burstCost: 60,
+  talentNames: {
+    normal: 'Favonius Bladework - Maid',
+    skill: 'Breastplate',
+    burst: 'Sweeping Time'
   },
-  "normal": [
+  normal: [
     {
-      "name": "1 - Hit DMG",
-      "tag": "N1",
-      "damageBonus": "normal",
-      "damage": [
+      name: '1 - Hit DMG',
+      tag: 'N1',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param1"
+            scaling: 'atk',
+            param: 'param1'
           }
         ]
       ]
     },
     {
-      "name": "2 - Hit DMG",
-      "tag": "N2",
-      "damageBonus": "normal",
-      "damage": [
+      name: '2 - Hit DMG',
+      tag: 'N2',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param2"
+            scaling: 'atk',
+            param: 'param2'
           }
         ]
       ]
     },
     {
-      "name": "3 - Hit DMG",
-      "tag": "N3",
-      "damageBonus": "normal",
-      "damage": [
+      name: '3 - Hit DMG',
+      tag: 'N3',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param3"
+            scaling: 'atk',
+            param: 'param3'
           }
         ]
       ]
     },
     {
-      "name": "4 - Hit DMG",
-      "tag": "N4",
-      "damageBonus": "normal",
-      "damage": [
+      name: '4 - Hit DMG',
+      tag: 'N4',
+      damageBonus: 'normal',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param4"
-          }
-        ]
-      ]
-    },
-    {
-      "name": "5 - Hit DMG",
-      "tag": "N5",
-      "damageBonus": "normal",
-      "damage": [
-        [
-          {
-            "scaling": "atk",
-            "param": "param5"
+            scaling: 'atk',
+            param: 'param4'
           }
         ]
       ]
     }
   ],
-  "charged": [
+  charged: [
     {
-      "name": "Charged Attack",
-      "tag": "CA",
-      "damageBonus": "charged",
-      "damage": [
+      name: 'Charged Attack Cyclic DMG',
+      tag: 'CA',
+      damageBonus: 'charged',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param6"
-          }
-        ]
-      ]
-    }
-  ],
-  "plunge": [
-    {
-      "name": "Plunge DMG",
-      "tag": "PD",
-      "damageBonus": "plunge",
-      "damage": [
-        [
-          {
-            "scaling": "atk",
-            "param": "param9"
+            scaling: 'atk',
+            param: 'param5'
           }
         ]
       ]
     },
     {
-      "name": "Low Plunge",
-      "tag": "PL",
-      "damageBonus": "plunge",
-      "damage": [
+      name: 'Charged Attack Final DMG',
+      tag: 'CF',
+      damageBonus: 'charged',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param10"
+            scaling: 'atk',
+            param: 'param6'
+          }
+        ]
+      ]
+    }
+  ],
+  plunge: [
+    {
+      name: 'Plunge DMG',
+      tag: 'PD',
+      damageBonus: 'plunge',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param9'
           }
         ]
       ]
     },
     {
-      "name": "High Plunge",
-      "tag": "PH",
-      "damageBonus": "plunge",
-      "damage": [
+      name: 'Low Plunge',
+      tag: 'PL',
+      damageBonus: 'plunge',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param11"
+            scaling: 'atk',
+            param: 'param10'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'High Plunge',
+      tag: 'PH',
+      damageBonus: 'plunge',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param11'
           }
         ]
       ]
     }
   ],
-  "skill": [
+  skill: [
     {
-      "name": "Skill DMG",
-      "tag": "EP",
-      "damageBonus": "skill",
-      "damage": [
+      name: 'Skill DMG',
+      tag: 'E',
+      damageBonus: 'skill',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param3"
+            scaling: 'def',
+            param: 'param6'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'DMG Absorption',
+      isShield: true,
+      damageBonus: 'skill',
+      damage: [
+        [
+          {
+            scaling: 'def',
+            param: 'param1'
+          },
+          {
+            scaling: 'flatValue',
+            param: 'param7'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Healing',
+      isHealing: true,
+      damageBonus: 'skill',
+      damage: [
+        [
+          {
+            scaling: 'def',
+            param: 'param2'
+          },
+          {
+            scaling: 'flatValue',
+            param: 'param8'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Devotion',
+      isShield: true,
+      damageBonus: 'skill',
+      damage: [
+        [
+          {
+            scaling: 'def',
+            param: 'param1',
+            coef: 4.0
+          }
+        ]
+      ]
+    },
+    {
+      name: 'To Be Cleaned DMG',
+      constellation: 4,
+      tag: 'X4',
+      damageBonus: 'skill',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param1',
+            coef: 4.0
           }
         ]
       ]
     }
   ],
-  "burst": [
+  burst: [
     {
-      "name": "Burst DMG",
-      "tag": "Q1",
-      "multiHit": 9,
-      "damageBonus": "burst",
-      "damage": [
+      name: 'Burst DMG',
+      tag: 'Q1',
+      damageBonus: 'burst',
+      damage: [
         [
           {
-            "scaling": "atk",
-            "param": "param1"
+            scaling: 'atk',
+            param: 'param1'
+          }
+        ]
+      ]
+    },
+    {
+      name: 'Skill DMG',
+      tag: 'Q2',
+      damageBonus: 'burst',
+      damage: [
+        [
+          {
+            scaling: 'atk',
+            param: 'param2'
           }
         ]
       ]
     }
   ],
-  "actions": [
+  actions: [
     {
-      "name": "Uprising Whirlwind",
-      "url": "UI_Talent_S_PlayerWind_02",
-      "description": "description",
-      "level": 1,
-      "constellation": 2,
-      "target": "self",
-      "actionType": "passive",
-      "values": [
+      name: 'Sweeping Time',
+      url: 'Skill_E_Noel_01',
+      description:
+        'Gathering the strength of stone around her weapon, Noelle strikes the opponents surrounding her within a large AoE, dealing Geo DMG.\nAfterwards, Noelle gains the following effects:\n·Larger attack AoE.\n·Converts attack DMG to Geo DMG that cannot be overridden by any other elemental infusion.\n·Increased ATK that scales based on her DEF.',
+      target: 'self',
+      infusion: 'geo',
+      hasLevels: 'burst',
+      sourceStats: ['def'],
+      actionType: 'toggle',
+      values: [
         {
-          "scaling": "energy",
-          "coef": 0
+          scaling: 'atk',
+          coef: 'param3',
+          source: ['def', 0]
+        }
+      ]
+    },
+    {
+      name: 'Combat Maid',
+      url: 'UI_Talent_S_Noel_02',
+      description:
+        "Decreases the Stamina Consumption of Noelle's Charged Attacks by 20% and increases her Charged Attack DMG by 15%.",
+      constellation: 2,
+      target: 'self',
+      actionType: 'passive',
+      values: [
+        {
+          scaling: 'charged',
+          coef: 0.15
+        }
+      ]
+    },
+    {
+      name: 'Must Be Spotless',
+      url: 'UI_Talent_S_Noel_04',
+      description:
+        "**Sweeping Time** increases Noelle's ATK by an additional 50% of her DEF.\nAdditionally, every opponent defeated during the skill's duration adds 1s to the duration, up to 10s.",
+      constellation: 6,
+      target: 'self',
+      sourceStats: ['def'],
+      actionType: 'toggle',
+      values: [
+        {
+          scaling: 'atk',
+          coef: 0.5,
+          source: ['def', 0]
         }
       ]
     }
-  ],
-  "offField": []
+  ]
 };
 
 export default charName;
