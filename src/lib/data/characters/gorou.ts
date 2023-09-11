186c555bf37780a0aea5e015a64d9eaa5eaf0254
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-09-11 07:10:28 -0400

    Update: Gorou completely finished 🛠✅

--- src/lib/data/characters/gorou.ts
+++ src/lib/data/characters/gorou.ts
@@ -201,24 +201,26 @@ const charName: CharacterRecord = {
     }
   ],
   actions: [
-    // {
-    //   "name": "General's War Banner",
-    //   "url": "Skill_S_Gorou_01",
-    //   "description": "Provides up to 3 buffs to active characters within the skill's AoE based on the number of Geo characters in the party at the time of casting:\n· 1 Geo character: Adds \"Standing Firm\" - DEF Bonus.\n· 2 Geo characters: Adds \"Impregnable\" - Increased resistance to interruption.\n· 3 Geo characters: Adds \"Crunch\" - Geo DMG Bonus.\n\nGorou can deploy only 1 General's War Banner on the field at any one time. Characters can only benefit from 1 General's War Banner at a time. When a party member leaves the field, the active buff will last for 2s.\n\n**Hold**\nAdjust the location of the skill.",
-    //   "target": "self",
-    //   "actionType": "toggle",
-    //   "hasLevels":'skill',
-    //   "values": [
-    //     {
-    //       "scaling": "def",
-    //       "coef": 'param2'
-    //     },
-    //     {
-    //       "scaling": "geo",
-    //       "coef": 0.15
-    //     }
-    //   ]
-    // },
+    {
+      name: "General's War Banner",
+      url: 'Skill_S_Gorou_01',
+      description:
+        'Provides up to 3 buffs to active characters within the skill\'s AoE based on the number of Geo characters in the party at the time of casting:\n· 1 Geo character: Adds "Standing Firm" - DEF Bonus.\n· 2 Geo characters: Adds "Impregnable" - Increased resistance to interruption.\n· 3 Geo characters: Adds "Crunch" - Geo DMG Bonus.\n\nGorou can deploy only 1 General\'s War Banner on the field at any one time. Characters can only benefit from 1 General\'s War Banner at a time. When a party member leaves the field, the active buff will last for 2s.\n\n**Hold**\nAdjust the location of the skill.',
+      target: 'active',
+      actionType: 'visionMatchToggle',
+      visionCondition: 'same',
+      hasLevels: 'skill',
+      values: [
+        {
+          scaling: 'def',
+          coef: ['param2', 'param2', 'param2']
+        },
+        {
+          scaling: 'geo',
+          coef: [0, 0, 0.15]
+        }
+      ]
+    },
     {
       name: 'Heedless of the Wind and Weather',
       url: 'UI_Talent_S_Gorou_05',
@@ -254,21 +256,22 @@ const charName: CharacterRecord = {
           source: ['def', 0]
         }
       ]
+    },
+    {
+      name: 'Uprising Whirlwind',
+      url: 'UI_Talent_S_PlayerWind_02',
+      description: 'description',
+      constellation: 6,
+      target: 'party',
+      actionType: 'visionMatchToggle',
+      visionCondition: 'same',
+      values: [
+        {
+          scaling: 'geoCritDMG',
+          coef: [0.1, 0.2, 0.4]
+        }
+      ]
     }
-    // {
-    //   name: 'Uprising Whirlwind',
-    //   url: 'UI_Talent_S_PlayerWind_02',
-    //   description: 'description',
-    //   constellation: 6,
-    //   target: 'self',
-    //   actionType: 'passive',
-    //   values: [
-    //     {
-    //       scaling: 'energy',
-    //       coef: 0
-    //     }
-    //   ]
-    // }
   ],
   offField: []
 };
