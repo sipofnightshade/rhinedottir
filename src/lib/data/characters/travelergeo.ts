dc58459e19f477a6999e2b879ed0a29c5e3d82ea
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-09-17 15:45:01 -0400

    Update: Traveler - Geo finished ✅

--- src/lib/data/characters/travelergeo.ts
+++ src/lib/data/characters/travelergeo.ts
@@ -5,16 +5,16 @@ const charName: CharacterRecord = {
   fullName: 'Aether',
   rating: 5,
   weapon: 'sword',
-  region: 'mondstat',
+  region: 'other',
   vision: 'geo',
   c3: 'skill',
   c5: 'burst',
   specialized: 'atk%',
   burstCost: 60,
   talentNames: {
-    normal: 'Foreign Ironwind',
-    skill: 'Palm Vortex',
-    burst: 'Gust Surge'
+    normal: 'Foreign Rockblade',
+    skill: 'Starfell Sword',
+    burst: 'Wake of Earth'
   },
   normal: [
     {
@@ -48,10 +48,10 @@ const charName: CharacterRecord = {
       damage: [[{ scaling: 'atk', param: 'param5' }]]
     },
     {
-      name: 'Slitting DMG',
-      tag: 'A1',
+      name: 'Rockslide DMG',
+      tag: 'A4',
       damageBonus: 'normal',
-      hasOwnDMGType: 'anemo',
+      hasOwnDMGType: 'geo',
       damage: [[{ scaling: 'atk', param: 'param1', coef: 0.6 }]]
     }
   ],
@@ -64,16 +64,6 @@ const charName: CharacterRecord = {
         [{ scaling: 'atk', param: 'param6' }],
         [{ scaling: 'atk', param: 'param7' }]
       ]
-    },
-    {
-      name: '1 - Hit DMG',
-      damageBonus: 'charged',
-      damage: [[{ scaling: 'atk', param: 'param6' }]]
-    },
-    {
-      name: '2 - Hit DMG',
-      damageBonus: 'charged',
-      damage: [[{ scaling: 'atk', param: 'param7' }]]
     }
   ],
   plunge: [
@@ -98,122 +88,47 @@ const charName: CharacterRecord = {
   ],
   skill: [
     {
-      name: 'Press DMG',
-      tag: 'EP',
-      damageBonus: 'skill',
-      damage: [[{ scaling: 'atk', param: 'param3' }]]
-    },
-    {
-      name: 'Hold DMG - Total',
-      tag: 'EH',
-      damageBonus: 'skill',
-      damage: [
-        [{ scaling: 'atk', param: 'param4' }],
-        [{ scaling: 'atk', param: 'param1' }],
-        [{ scaling: 'atk', param: 'param1' }],
-        [{ scaling: 'atk', param: 'param1' }],
-        [{ scaling: 'atk', param: 'param2' }],
-        [{ scaling: 'atk', param: 'param2' }],
-        [{ scaling: 'atk', param: 'param2' }]
-      ]
-    },
-    {
-      name: 'Max Storm Damage',
-      damageBonus: 'skill',
-      damage: [[{ scaling: 'atk', param: 'param4' }]]
-    },
-    {
-      name: 'Initial Cutting DMG x3',
+      name: 'Skill DMG',
+      tag: 'E',
       damageBonus: 'skill',
       damage: [[{ scaling: 'atk', param: 'param1' }]]
     },
     {
-      name: 'Max Cutting DMG x3',
+      name: 'Explosion DMG',
+      tag: 'X2',
+      constellation: 2,
       damageBonus: 'skill',
-      damage: [[{ scaling: 'atk', param: 'param2' }]]
+      damage: [[{ scaling: 'atk', param: 'param1' }]]
     }
   ],
   burst: [
     {
-      name: 'Tornado DMG',
-      tag: 'Q1',
-      multiHit: 9,
+      name: 'Total Burst DMG',
+      tag: 'Q',
       damageBonus: 'burst',
-      damage: [[{ scaling: 'atk', param: 'param1' }]]
+      damage: [
+        [{ scaling: 'atk', param: 'param1' }],
+        [{ scaling: 'atk', param: 'param1' }],
+        [{ scaling: 'atk', param: 'param1' }],
+        [{ scaling: 'atk', param: 'param1' }]
+      ]
     },
     {
-      name: 'Additional Elemental DMG',
-      multiHit: 9,
-      tag: 'Q2',
+      name: 'DMG Per Shockwave',
       damageBonus: 'burst',
-      damage: [[{ scaling: 'atk', param: 'param2' }]]
+      damage: [[{ scaling: 'atk', param: 'param1' }]]
     }
   ],
   actions: [
     {
-      name: 'Uprising Whirlwind',
-      url: 'UI_Talent_S_PlayerWind_02',
+      name: 'Invincible Stonewall',
+      url: 'UI_Talent_S_PlayerRock_01',
       description:
-        'Guiding the path of the wind currents, you summon a forward-moving tornado that pulls objects and opponents towards itself, dealing continuous Anemo DMG.\n\n**Elemental Absorption**\nIf the tornado comes into contact with Hydro/Pyro/Cryo/Electro, it will deal additional elemental DMG of that type.\nElemental Absorption may only occur once per use.',
-      level: 2,
-      constellation: 2,
-      target: 'self',
-      actionType: 'passive',
-      values: [{ scaling: 'energy', coef: 0.16 }]
-    },
-    {
-      name: 'Intertwined Winds',
-      url: 'UI_Talent_S_PlayerWind_04',
-      description:
-        'Targets who take DMG from Gust Surge have their Anemo RES decreased by 20%. If an Elemental Absorption occurred, then their RES towards the corresponding Element is also decreased by 20%.',
-      level: 2,
-      constellation: 6,
-      target: 'enemy',
+        'Party members within the radius of **Wake of Earth** have their CRIT Rate increased by 10% and have increased resistance against interruption.',
+      constellation: 1,
+      target: 'party',
       actionType: 'toggle',
-      values: [{ scaling: 'anemoRes', coef: 0.2 }]
-    },
-    {
-      name: 'Intertwined Winds - Elemental Absorption',
-      url: 'UI_Talent_S_PlayerWind_04',
-      description:
-        'If an Elemental Absorption occurred, then their RES towards the corresponding Element is also decreased by 20%.',
-      level: 2,
-      constellation: 6,
-      target: 'enemy',
-      actionType: 'multiSelect',
-      values: [
-        { scaling: 'pyroRes', coef: 0.2 },
-        { scaling: 'hydroRes', coef: 0.2 },
-        { scaling: 'electroRes', coef: 0.2 },
-        { scaling: 'cryoRes', coef: 0.2 }
-      ]
-    },
-    {
-      name: 'Testing',
-      url: 'UI_Talent_S_PlayerWind_04',
-      description:
-        'If an Elemental Absorption occurred, then their RES towards the corresponding Element is also decreased by 20%.',
-      level: 2,
-      constellation: 6,
-      target: 'enemy',
-      actionType: 'stack',
-      values: [{ scaling: 'anemo', coef: [0.15, 0.05, 0.05, 0.05] }]
-    },
-    {
-      name: 'Testing2',
-      url: 'UI_Talent_S_PlayerWind_04',
-      description:
-        'If an Elemental Absorption occurred, then their RES towards the corresponding Element is also decreased by 20%.',
-      level: 2,
-      constellation: 6,
-      target: 'enemy',
-      actionType: 'select',
-      values: [
-        { scaling: 'electroRes', coef: 0.15 },
-        { scaling: 'pyroRes', coef: 0.05 },
-        { scaling: 'hydroRes', coef: 0.15 },
-        { scaling: 'cryoRes', coef: 0.05 }
-      ]
+      values: [{ scaling: 'critrate', coef: 0.1 }]
     }
   ]
 };
