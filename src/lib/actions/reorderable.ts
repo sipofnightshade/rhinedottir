13e8231157348d773be1a99122773c09a8f1ebe1
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-09-29 12:19:21 -0400

    Update: Cleanup event listeners in reorderable✅

--- src/lib/actions/reorderable.ts
+++ src/lib/actions/reorderable.ts
@@ -1,40 +1,54 @@
 // reorderDraggable.js
 function reorderable(node: HTMLElement) {
-  let selectedItem: any = null;
+  let selectedItem: HTMLElement | null = null;
 
-  node.addEventListener('dragstart', (event: any) => {
-    selectedItem = event.target;
-    event.dataTransfer.setData('text/plain', '');
-    selectedItem.classList.add('drag-sort-active');
-  });
+  function handleDragStart(event: DragEvent) {
+    if (event.target instanceof HTMLElement) {
+      selectedItem = event.target;
+      event.dataTransfer?.setData('text/plain', '');
+      selectedItem.classList.add('drag-sort-active');
+    }
+  }
 
-  node.addEventListener('dragover', (event: any) => {
+  function handleDragOver(event: DragEvent) {
     event.preventDefault();
     const x = event.clientX;
     const y = event.clientY;
-    const swapItem: any = document.elementFromPoint(x, y);
+    const swapItem = document.elementFromPoint(x, y) as HTMLElement | null;
 
-    if (selectedItem && selectedItem !== swapItem) {
-      const list = selectedItem.parentNode;
-      const items = Array.from(list.children);
+    if (selectedItem && swapItem && selectedItem !== swapItem) {
+      const list = selectedItem.parentNode as HTMLElement;
+      const items = Array.from(list.children) as HTMLElement[];
       const selectedIndex = items.indexOf(selectedItem);
       const swapIndex = items.indexOf(swapItem);
 
       if (selectedIndex !== -1 && swapIndex !== -1) {
         list.insertBefore(
           selectedItem,
-          swapIndex < selectedIndex ? swapItem : swapItem.nextSibling
+          swapIndex < selectedIndex ? swapItem : swapItem?.nextSibling
         );
       }
     }
-  });
+  }
 
-  node.addEventListener('dragend', () => {
+  function handleDragEnd() {
     if (selectedItem) {
       selectedItem.classList.remove('drag-sort-active');
       selectedItem = null;
     }
-  });
+  }
+
+  node.addEventListener('dragstart', handleDragStart);
+  node.addEventListener('dragover', handleDragOver);
+  node.addEventListener('dragend', handleDragEnd);
+
+  return {
+    destroy() {
+      node.removeEventListener('dragstart', handleDragStart);
+      node.removeEventListener('dragover', handleDragOver);
+      node.removeEventListener('dragend', handleDragEnd);
+    }
+  };
 }
 
 export default reorderable;
