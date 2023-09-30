0bae81721731cfcc9399a64b2350fba0d345fea3
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-09-30 16:51:58 -0400

    New: added global horizontal scrollbar styles ✅

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -40,5 +40,22 @@
       border-radius: 4px;
       background-color: #334155;
     }
+
+    :global(.horizontalScrollbar)::-webkit-scrollbar {
+      width: 8px;
+      height: 5px;
+      margin-top: 10px;
+    }
+
+    :global(.horizontalScrollbar)::-webkit-scrollbar-track {
+      border-radius: 12px;
+      background-color: #e2e8f0;
+      padding: 1px;
+    }
+
+    :global(.horizontalScrollbar)::-webkit-scrollbar-thumb {
+      border-radius: 4px;
+      background-color: #334155;
+    }
   }
 </style>
