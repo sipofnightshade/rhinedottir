fc38370b7be0ed1d35b1b08cfb383c0c1a8e6f22
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-10-02 23:42:17 -0400

    🔥New: Row now uses CptnCodeman popover

--- src/components/Modal/Combo/Row.svelte
+++ src/components/Modal/Combo/Row.svelte
@@ -1,30 +1,32 @@
 <script lang="ts">
-  import TalentSection from './TalentSection.svelte';
   import { writable } from 'svelte/store';
   import { SortableList } from '@jhubbardsf/svelte-sortablejs';
-
-  // misc
   import { talents } from '$lib/stores/talentStore';
-  // import { onMount } from 'svelte';
+  import { createPopover } from 'svelte-headlessui';
+  import Transition from 'svelte-transition';
 
   // components
   import ComboAddButton from './ComboAddButton.svelte';
   import ComboButton from './ComboButton.svelte';
   import ShortModal from '../ShortModal.svelte';
   import RowHeading from './RowHeading.svelte';
+  import TalentSection from './TalentSection.svelte';
 
+  // types
   type CharacterID = 'main' | 'one' | 'two' | 'three';
   type TalentType = 'normal' | 'charged' | 'plunge' | 'special' | 'skill' | 'burst';
-
-  export let row: any;
-
   type Buttons = { id: CharacterID; type: TalentType; index: number; btnID: string }[];
+
+  // props
+  export let row: any;
 
   // main functionality
   let dialog: HTMLDialogElement;
   let rowButtons: Buttons = [];
   let totalDamage = writable(0);
   let deletable = false;
+
+  const popover = createPopover({});
 
   function addButton(event: CustomEvent) {
     const { index, id, type, btnID } = event.detail;
@@ -62,7 +64,7 @@
   }
 </script>
 
-<section class="my-2 w-full border-b border-slate-700 pb-2">
+<section class="relative my-2 w-full border-b border-slate-700 pb-2">
   <RowHeading title={row.title} />
 
   <div class="flex w-full items-center justify-start">
@@ -81,8 +83,60 @@
           on:removeBtn={removeButton}
         />
       {/each}
-      <ComboAddButton on:click={handleComboAddButton} {deletable} />
+      <!-- <ComboAddButton on:click={handleComboAddButton} {deletable} /> -->
+      <button
+        use:popover.button
+        class="mr-1 flex h-8 w-8 min-w-[32px] items-center justify-center rounded-full bg-slate-700"
+        title={deletable ? 'Clear All' : 'Add Buttons!'}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-4 w-4 transition-transform"
+          class:rotate-45={deletable}
+          viewBox="0 0 448 512"
+          class:fill-slate-300={!deletable}
+          class:fill-red-500={deletable}
+        >
+          <path
+            d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"
+          />
+        </svg>
+      </button>
     </SortableList>
+    <Transition
+      show={$popover.expanded}
+      enter="transition ease-out duration-200"
+      enterFrom="opacity-0 translate-y-1"
+      enterTo="opacity-100 translate-y-0"
+      leave="transition ease-in duration-150"
+      leaveFrom="opacity-100 translate-y-0"
+      leaveTo="opacity-0 translate-y-1"
+    >
+      <!-- svelte-ignore a11y-no-static-element-interactions -->
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <div
+        use:popover.panel
+        class="absolute left-1/2 top-24 z-10 mt-3 w-screen max-w-xs -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl"
+      >
+        <!-- <button on:click={popover.close}>Close</button> -->
+        <div class="rounded-xl border border-slate-500 bg-slate-800 p-4">
+          <TalentSection on:addbutton={addButton} type="normal" id="main" />
+          <TalentSection on:addbutton={addButton} type="charged" id="main" />
+          <TalentSection on:addbutton={addButton} type="plunge" id="main" />
+          <TalentSection on:addbutton={addButton} type="skill" id="main" />
+          <TalentSection on:addbutton={addButton} type="burst" id="main" />
+
+          <TalentSection on:addbutton={addButton} type="skill" id="one" />
+          <TalentSection on:addbutton={addButton} type="burst" id="one" />
+
+          <TalentSection on:addbutton={addButton} type="skill" id="two" />
+          <TalentSection on:addbutton={addButton} type="burst" id="two" />
+
+          <TalentSection on:addbutton={addButton} type="skill" id="three" />
+          <TalentSection on:addbutton={addButton} type="burst" id="three" />
+        </div>
+      </div>
+    </Transition>
   </div>
   <div class="mx-0.5 mt-1 flex items-center justify-between">
     <div class="flex gap-1">
