499ea768804c0087355ccdda2900d6f8fefb7ae3
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-10-07 11:33:32 -0400

    Update: Added party actions to ActionBar ✅

--- src/components/Actions/ActionBar.svelte
+++ src/components/Actions/ActionBar.svelte
@@ -44,4 +44,75 @@
     currentChar={$character}
     currentStats={$stats.main}
   />
+
+  <!-- Party one -->
+  {#if $party.one && $stats.one}
+    {#each $party.one.character.selected.actions as data (data.name)}
+      <CharacterButton
+        {data}
+        id="one"
+        currentChar={$party.one.character}
+        currentStats={$stats.one}
+      />
+    {/each}
+    <ArtifactButton
+      setData={$party.one.artifacts}
+      id="one"
+      currentChar={$party.one.character}
+      currentStats={$stats.one}
+    />
+    <WeaponButton
+      currentWeapon={$party.one.weapon}
+      id="one"
+      currentChar={$party.one.character}
+      currentStats={$stats.one}
+    />
+  {/if}
+
+  <!-- Party two -->
+  {#if $party.two && $stats.two}
+    {#each $party.two.character.selected.actions as data (data.name)}
+      <CharacterButton
+        {data}
+        id="two"
+        currentChar={$party.two.character}
+        currentStats={$stats.two}
+      />
+    {/each}
+    <ArtifactButton
+      setData={$party.two.artifacts}
+      id="two"
+      currentChar={$party.two.character}
+      currentStats={$stats.two}
+    />
+    <WeaponButton
+      currentWeapon={$party.two.weapon}
+      id="two"
+      currentChar={$party.two.character}
+      currentStats={$stats.two}
+    />
+  {/if}
+  <!-- Party three -->
+  {#if $party.three && $stats.three}
+    {#each $party.three.character.selected.actions as data (data.name)}
+      <CharacterButton
+        {data}
+        id="three"
+        currentChar={$party.three.character}
+        currentStats={$stats.three}
+      />
+    {/each}
+    <ArtifactButton
+      setData={$party.three.artifacts}
+      id="three"
+      currentChar={$party.three.character}
+      currentStats={$stats.three}
+    />
+    <WeaponButton
+      currentWeapon={$party.three.weapon}
+      id="three"
+      currentChar={$party.three.character}
+      currentStats={$stats.three}
+    />
+  {/if}
 </section>
