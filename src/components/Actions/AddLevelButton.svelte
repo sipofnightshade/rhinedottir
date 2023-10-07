b81364d78dd263465212fbe70e59ab09873691c0
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-10-06 21:04:29 -0400

    New: AddLevelButton ✅

---
+++ src/components/Actions/AddLevelButton.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+  // types
+  import type { Action, ActionBtnID } from '$lib/types/actions';
+  import type { Visions } from '$lib/types/global';
+  import type { Index_Stats } from '$lib/data/Stats';
+
+  // components
+  import ActionButton from './ActionButton.svelte';
+
+  // other
+  import { onMount } from 'svelte';
+  import { character, type CurrentCharacter } from '$lib/stores/characterStore';
+  import { party } from '$lib/stores/partyStore';
+
+  // props
+  export let type: Visions | 'weapon' | 'artifact';
+  export let data: Action;
+  export let id: ActionBtnID;
+  export let currentChar: CurrentCharacter;
+  export let currentStats: Index_Stats;
+
+  let isInitialized = false;
+
+  const target = data.target;
+
+  type Talents = 'atk' | 'skill' | 'burst';
+
+  function addLevel() {
+    // may need to add the coef to party as well later on
+    data.values.forEach((value) => {
+      const { scaling, coef } = value;
+
+      if (target === 'self') {
+        $character.lvlBonus[scaling as Talents] += coef as number;
+      } else if (target === 'party') {
+        $character.lvlBonus[scaling as Talents] += coef as number;
+
+        if ($party.one) {
+          $party.one.character.lvlBonus[scaling as Talents] += coef as number;
+        }
+
+        if ($party.two) {
+          $party.two.character.lvlBonus[scaling as Talents] += coef as number;
+        }
+
+        if ($party.three) {
+          $party.three.character.lvlBonus[scaling as Talents] += coef as number;
+        }
+      }
+    });
+  }
+
+  function removeLevel() {
+    // may need to add the coef to party as well later on
+    data.values.forEach((value) => {
+      const { scaling, coef } = value;
+
+      if (target === 'self') {
+        $character.lvlBonus[scaling as Talents] -= coef as number;
+      } else if (target === 'party') {
+        $character.lvlBonus[scaling as Talents] -= coef as number;
+
+        if ($party.one) {
+          $party.one.character.lvlBonus[scaling as Talents] -= coef as number;
+        }
+
+        if ($party.two) {
+          $party.two.character.lvlBonus[scaling as Talents] -= coef as number;
+        }
+
+        if ($party.three) {
+          $party.three.character.lvlBonus[scaling as Talents] -= coef as number;
+        }
+      }
+    });
+  }
+
+  onMount(() => {
+    addLevel();
+    // this return might be problematic if the state is reset when character changes
+    // because then this might run after and substract values that were not added
+    isInitialized = true;
+
+    return () => {
+      removeLevel();
+    };
+  });
+</script>
+
+<button on:contextmenu={() => console.log(data.name)} data-testid="passive-action-button">
+  <ActionButton {type} url={data.url} isActive />
+</button>
