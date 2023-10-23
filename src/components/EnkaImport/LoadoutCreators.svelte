193b1d7b7b8e9f04be28b981ebb4a5637fe0e881
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-10-23 16:08:21 -0400

    🌟New: LoadoutCreators to create builds ✅

---
+++ src/components/EnkaImport/LoadoutCreators.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import ImportButton from './ImportButton.svelte';
+  export let playerBuilds: any[];
+</script>
+
+<div class="mt-2">
+  <h3>Select to import:</h3>
+  <div class="mt-2 grid grid-cols-8 gap-2">
+    {#each playerBuilds as build (build.character.selected)}
+      <ImportButton {build} />
+    {/each}
+  </div>
+</div>
