d260523925f584d348b7619834b766ac651be94e
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-10-25 12:04:48 -0400

    🍦New: Installed Melt UI ✅

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
+import { preprocessMeltUI } from '@melt-ui/pp';
+import sequence from 'svelte-sequential-preprocessor';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/kit').Config}*/
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
-
+  preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
   kit: {
     adapter: adapter({
       // default options are shown. On some platforms
@@ -19,5 +19,4 @@ const config = {
     })
   }
 };
-
 export default config;
