ffbdfc52e2e62c345e87cf15b37ebabe6869269d
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-11-01 09:36:41 -0400

    Minor: Added comments to infusion store

--- src/lib/stores/infusionStore.ts
+++ src/lib/stores/infusionStore.ts
@@ -5,6 +5,14 @@ import type { DamageType } from '$lib/types/global';
 function createInfusion() {
   const { subscribe, set, update } = writable<DamageType>('physical');
 
+  /**
+   * @todo
+   * Change infusion property to accept an array/object,
+   * --> eg: infusion: ['pyro','self'] or infusion: ['pyro','melee']
+   * Return an object from this store for each ID and they will contain
+   * the appropriate infusions.
+   */
+
   return {
     subscribe,
     setInfusion: (
@@ -22,6 +30,30 @@ function createInfusion() {
 
         return state;
       }),
+    // setInfusion2: (
+    //   infusion: [DamageType, 'self' | 'melee'],
+    //   id: 'main' | 'one' | 'two' | 'three'
+    // ) =>
+    //   update((state) => {
+    //     // Keep current infusion if self infused
+    //     if (state[id] !== 'physical') return state;
+
+    //     switch (infusion[1]) {
+    //       case 'self':
+    //         state[id] = infusion;
+    //         break;
+    //       case 'melee':
+    //         state.main = infusion;
+    //         state.one = infusion;
+    //         state.two = infusion;
+    //         state.three = infusion;
+    //         break;
+    //       default:
+    //         break;
+    //     }
+
+    //     return state;
+    //   }),
     reset: () => set('physical')
   };
 }
