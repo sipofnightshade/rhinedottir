a5b90791fe8da1f1bfa80e3b4b329e9f7514be3d
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-11-02 09:22:46 -0400

    Update: Customized enemy stat table ✅

---
+++ src/components/TableRow/EnemyStatRow.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Cell from './Cell.svelte';
+
+  export let label: string;
+  export let value: number = 0;
+  export let multiplier = true;
+
+  // $: displayValue = Math.round(value * 100) / 100;
+</script>
+
+<div class="grid grid-cols-20 px-0.5 py-1 text-tb">
+  <Cell align="start" col="col-span-12" value={label} />
+  <Cell
+    align="end"
+    col="col-span-8"
+    value={multiplier ? 'x' + value.toFixed(2) : value}
+  />
+</div>
