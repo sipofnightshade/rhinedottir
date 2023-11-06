43af8e4745df0bfa9ae53f2fd6e90943e6533ed4
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-11-06 15:31:49 -0400

    🌟Fix: reverted reactions and adjusted ICD check ✅

--- src/lib/calculators/calcFinalDMG.ts
+++ src/lib/calculators/calcFinalDMG.ts
@@ -9,10 +9,11 @@ import { calcCritStat } from './calcCritStat';
 import { calcDEFMultiplier } from './calcDEFMultiplier';
 import { calcDMGBonus } from './calcDMGBonus';
 import { calcDamageNoReaction } from './calcDamageNoReaction';
-import { calcFinalReactionDMG } from './calcFinalReactionDMG';
 import { calcHealing } from './calcHealing';
 import { calcHitDamage } from './calcHitDamage';
 import { calcShield } from './calcShield';
+import { calcTransforming } from './calcTransforming';
+import { calcAmplifying } from './calcAmplifyingMultiplier';
 
 export function calcFinalDMG(
   hit: Hit,
@@ -33,19 +34,19 @@ export function calcFinalDMG(
       ...hit,
       elemental: 'healing',
       damage: {
-        base: healing
+        healing
       }
     };
   }
   // if this hit is a shield, then return only the healing values
 
   if (hit.isShield) {
-    const shielding = calcShield(hit.damage[0], $stats, values, talentLvl);
+    const shield = calcShield(hit.damage[0], $stats, values, talentLvl);
     return {
       ...hit,
       elemental: 'shield',
       damage: {
-        base: shielding
+        shield
       }
     };
   }
@@ -116,7 +117,6 @@ export function calcFinalDMG(
         critRate,
         critDMG
       );
-      total.base += result;
 
       const catalyzeResult = calcDamageNoReaction(
         hitDMG,
@@ -130,30 +130,179 @@ export function calcFinalDMG(
         critDMG
       );
 
-      if (element === 'physical') {
-        return total;
-      } else {
-        return {
-          base: total.base,
-          ...calcFinalReactionDMG(
-            i,
-            element,
-            $stats,
-            $character.lvl,
-            result,
-            $enemy,
-            ICD,
-            catalyzeResult
-          )
-        };
+      total.base += result;
+      total[element] += result;
+
+      /**
+       * @here Conditionally add reactions to total
+       */
+      if (element === 'electro') {
+        // initialize reaction values to 0 if undefined
+        total.aggravate = total.aggravate || 0;
+        total.superconduct = total.superconduct || 0;
+        total.electrocharged = total.electrocharged || 0;
+        total.overloaded = total.overloaded || 0;
+        // if damage instance is <ON> cooldown, add reactions
+        if (i === 0 || i % ICD === 0) {
+          total.aggravate += catalyzeResult;
+          total.superconduct +=
+            calcTransforming(
+              'superconduct',
+              $stats.em,
+              $character.lvl,
+              $stats.superconduct,
+              $enemy.cryo
+            ) + result;
+          total.electrocharged +=
+            calcTransforming(
+              'electrocharged',
+              $stats.em,
+              $character.lvl,
+              $stats.electrocharged,
+              $enemy.electro
+            ) + result;
+          total.overloaded +=
+            calcTransforming(
+              'overloaded',
+              $stats.em,
+              $character.lvl,
+              $stats.overloaded,
+              $enemy.pyro
+            ) + result;
+        } else {
+          // if damage instance is <OFF> cooldown, add base damage
+          total.aggravate += result;
+          total.superconduct += result;
+          total.electrocharged += result;
+        }
       }
+      if (element === 'dendro') {
+        total.spread = total.spread || 0;
+        if (i === 0 || i % ICD === 0) {
+          total.spread += catalyzeResult;
+        } else {
+          total.spread += result;
+        }
+      }
+      if (element === 'anemo') {
+        // total.swirl = total.swirl || 0;
+        total.pyroSwirl = total.pyroSwirl || 0;
+        total.cryoSwirl = total.cryoSwirl || 0;
+        total.electroSwirl = total.electroSwirl || 0;
+        total.hydroSwirl = total.hydroSwirl || 0;
+        if (i === 0 || i % ICD === 0) {
+          // total.swirl +=
+          //   calcTransforming('swirl', $stats.em, $character.lvl, $stats.swirl, $enemy.anemo) +
+          //   result;
+          total.pyroSwirl +=
+            calcTransforming(
+              'swirl',
+              $stats.em,
+              $character.lvl,
+              $stats.swirl,
+              $enemy.pyro
+            ) + result;
+          total.cryoSwirl +=
+            calcTransforming(
+              'swirl',
+              $stats.em,
+              $character.lvl,
+              $stats.swirl,
+              $enemy.cryo
+            ) + result;
+          total.electroSwirl +=
+            calcTransforming(
+              'swirl',
+              $stats.em,
+              $character.lvl,
+              $stats.swirl,
+              $enemy.electro
+            ) + result;
+          total.hydroSwirl +=
+            calcTransforming(
+              'swirl',
+              $stats.em,
+              $character.lvl,
+              $stats.swirl,
+              $enemy.hydro
+            ) + result;
+        } else {
+          total.pyroSwirl += result;
+          total.cryoSwirl += result;
+          total.electroSwirl += result;
+          total.hydroSwirl += result;
+        }
+      }
+      if (element === 'pyro') {
+        total.vaporize = total.vaporize || 0;
+        total.melt = total.melt || 0;
+        total.overloaded = total.overloaded || 0;
+        if (i === 0 || i % ICD === 0) {
+          total.vaporize += result * calcAmplifying(1.5, $stats.em, $stats.vaporize);
+          total.melt += result * calcAmplifying(2, $stats.em, $stats.melt);
+          total.overloaded +=
+            calcTransforming(
+              'overloaded',
+              $stats.em,
+              $character.lvl,
+              $stats.overloaded,
+              $enemy.pyro
+            ) + result;
+        } else {
+          total.vaporize += result;
+          total.melt += result;
+          total.overloaded += result;
+        }
+      }
+      if (element === 'hydro') {
+        total.vaporize = total.vaporize || 0;
+        total.electrocharged = total.electrocharged || 0;
+        if (i === 0 || i % ICD === 0) {
+          total.vaporize += result * calcAmplifying(2, $stats.em, $stats.vaporize);
+          total.electrocharged +=
+            calcTransforming(
+              'electrocharged',
+              $stats.em,
+              $character.lvl,
+              $stats.electrocharged,
+              $enemy.electro
+            ) + result;
+        } else {
+          total.vaporize += result;
+          total.electrocharged += result;
+        }
+      }
+      if (element === 'cryo') {
+        total.melt = total.melt || 0;
+        total.superconduct = total.superconduct || 0;
+        if (i === 0 || i % ICD === 0) {
+          total.melt += result * calcAmplifying(1.5, $stats.em, $stats.melt);
+          total.superconduct +=
+            calcTransforming(
+              'superconduct',
+              $stats.em,
+              $character.lvl,
+              $stats.superconduct,
+              $enemy.cryo
+            ) + result;
+        } else {
+          total.melt += result;
+          total.superconduct += result;
+        }
+      }
+
+      return total;
     },
-    { base: 0 }
+    { base: 0, [element]: 0 }
   );
+
+  const newDMG = Object.entries(FinalDMG).map(([type, value]) => ({ type, value }));
 
   return {
     ...hit,
+    numOfHits: hit.damage.length,
     elemental: element,
-    damage: { ...FinalDMG }
+    damage: { ...FinalDMG },
+    newDMG
   };
 }
