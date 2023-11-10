34597fcff2d40669a844bfb1bc23a1530fe19958
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-11-10 10:17:07 -0400

    Minor: Adjusted sizing of StarSelector

--- src/components/Stars/StarSelector.svelte
+++ src/components/Stars/StarSelector.svelte
@@ -27,7 +27,7 @@
 
 <button
   on:click={() => artifact.setRating(type)}
-  class="flex h-10 w-full items-center justify-between gap-1 rounded-lg bg-slate-700 p-3"
+  class="flex h-10 w-32 items-center justify-between rounded-lg bg-slate-700 p-3"
   class:opacity-0={$artifact[type].selected.name === 'none'}
   disabled={$artifact[type].selected.name === 'none'}
 >
@@ -36,7 +36,7 @@
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 28 26"
-        class="h-4 w-4 fill-current {item}"
+        class="h-3.5 w-3.5 fill-current {item}"
       >
         <path
           d="M13.05.93a1 1 0 0 1 1.9 0l2.42 7.43a1 1 0 0 0 .95.7h7.82a1 1 0 0 1 .59 1.8l-6.33 4.6a1 1 0 0 0-.36 1.12l2.41 7.44a1 1 0 0 1-1.54 1.12l-6.32-4.6a1 1 0 0 0-1.18 0l-6.32 4.6a1 1 0 0 1-1.54-1.12l2.41-7.44a1 1 0 0 0-.36-1.12l-6.33-4.6a1 1 0 0 1 .6-1.8h7.81a1 1 0 0 0 .95-.7L13.05.93Z"
