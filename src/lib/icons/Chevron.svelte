6ae68332516aa162277bd8731d94ac5d4e3f62e8
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-11-10 00:29:13 -0400

    🌟Update: Added rotation to chevron icon ✅

--- src/lib/icons/Chevron.svelte
+++ src/lib/icons/Chevron.svelte
@@ -1,4 +1,13 @@
-<svg xmlns="http://www.w3.org/2000/svg" class={$$props.class} viewBox="0 0 512 512">
+<script lang="ts">
+  export let flip = false;
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  class="transition-transform duration-[400ms] {$$props.class}"
+  class:rotate-180={flip}
+  viewBox="0 0 512 512"
+>
   <path
     d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"
   />
