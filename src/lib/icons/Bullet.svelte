ca0effa3c18d35483fa11b7feb8d6940c6869c1c
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-11-10 14:54:58 -0400

    New: added Bullet icon component ✅

---
+++ src/lib/icons/Bullet.svelte
@@ -0,0 +1,8 @@
+<svg class={$$props.class} viewBox="0 0 64 65" xmlns="http://www.w3.org/2000/svg">
+  <path d="M44.263 32.6863L32 20.4233L19.737 32.6863L32 44.9493L44.263 32.6863Z" />
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M32 0.686279L0 32.6863L32 64.6863L64 32.6863L32 0.686279ZM32 12.2479L11.5617 32.6863L32 53.1246L52.4383 32.6863L32 12.2479Z"
+  />
+</svg>
