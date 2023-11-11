1032d3e708859bf3deb6fcc677dbb469754d37b4
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-11-10 21:40:01 -0400

    🌟New: created MultiModal ✅

---
+++ src/components/MultiModal/MultiModal.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import Close from '$lib/icons/Close.svelte';
+
+  export let dialog: HTMLDialogElement;
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<dialog
+  bind:this={dialog}
+  on:click|self={() => dialog.close()}
+  class="h-full max-w-md bg-slate-800 text-slate-200 shadow-lg backdrop:bg-black/60 xs-300:p-2 xs:h-[90vh] xs:rounded-2xl xs:p-4"
+>
+  <!-- Dialog Container starts here -->
+  <div class="flex h-full flex-col rounded-xl">
+    <header class="flex w-full justify-between border-b border-slate-600 p-2 xs:p-4">
+      <slot name="title" />
+      <button on:click={() => dialog.close()}>
+        <Close class="w-3 fill-slate-100" />
+      </button>
+    </header>
+    <div class="h-full overflow-hidden p-1 xs-300:p-2 xs:p-4">
+      <slot />
+    </div>
+    {#if $$slots.footer}
+      <footer class="flex w-full justify-between border-t border-slate-600 p-2 xs:p-4">
+        <slot name="footer" />
+      </footer>
+    {/if}
+  </div>
+</dialog>
+
+<style lang="postcss">
+  dialog {
+    padding: 0;
+  }
+</style>
