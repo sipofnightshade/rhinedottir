f65b6adedb890e79e2d51ec1b26f022881ef15e3
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-11-10 22:12:17 -0400

    Minor: adjusted Filters style

--- src/components/Filters/Filters.svelte
+++ src/components/Filters/Filters.svelte
@@ -14,10 +14,10 @@
   {#each filters as filter}
     <button
       on:click={() => setFilter(filter)}
-      class="relative flex aspect-square items-center justify-center rounded-lg"
-      class:bg-slate-500={filter === selected}
+      class="relative flex aspect-square items-center justify-center rounded-lg transition-colors hover:bg-slate-700/50"
+      class:bg-slate-700={filter === selected}
     >
-      <img src="/images/elements/{filter}.svg" alt={filter} class="w-8" />
+      <img src="/images/elements/{filter}.svg" alt={filter} class="w-6" />
     </button>
   {/each}
 </div>
