8ad9ddade8443cdd24a3e5fe611f24e79b9b170e
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-11-24 11:28:54 -0400

    🌟New: Created `getImageURL` function ✅

---
+++ src/lib/helpers/getImageURL.ts
@@ -0,0 +1,21 @@
+import type { ArtifactType } from '$lib/types/artifacts';
+
+type ImageType = 'character' | 'weapon' | ArtifactType;
+const artifactID = {
+  flower: 4,
+  feather: 2,
+  sands: 5,
+  goblet: 1,
+  circlet: 3
+};
+export function getImageUrl(type: ImageType, url: string, name: string) {
+  if (type === 'character' || type === 'weapon') {
+    return `https://enka.network/ui/${url}.png`;
+  }
+
+  if (name !== 'none' && artifactID[type]) {
+    return `https://enka.network/ui/${url}_${artifactID[type]}.png`;
+  } else {
+    return `/images/artifact/${type}/${name}.webp`;
+  }
+}
