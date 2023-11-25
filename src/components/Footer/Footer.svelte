eb6bb6fc79892b2dbe2178b93c92636dd1f2f91e
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-11-24 21:45:39 -0400

    Updated footer ✅

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -1,7 +1,7 @@
 <footer class="flex justify-between">
-  <p>
+  <p class="text-sm">
     © All rights reserved by Hoyoverse. Other properties belong to their respective
     owners.
   </p>
-  <p>Genshin Impact Version 3.2 | Rhinedottir Version 1.0</p>
+  <p class="text-sm">BETA</p>
 </footer>
