81e0ce95da4e68ea0dd516189e8ee4ba696a498a
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-11-26 22:04:51 -0400

    ❗FIX: actionbar scroll by removing `touch-none`✅

--- src/components/Actions/ActionButton.svelte
+++ src/components/Actions/ActionButton.svelte
@@ -48,47 +48,47 @@
 
 <span class="hidden">{type} Button</span>
 <div
-  class="relative flex h-16 w-16 flex-shrink-0 touch-none items-center justify-center"
+  class="relative flex h-16 w-16 flex-shrink-0 items-center justify-center"
   class:grayscale-[30%]={!isActive}
   class:opacity-50={!isActive}
 >
   <div
-    class="pointer-events-none absolute h-12 w-12 touch-none rounded-full {backgrounds[
+    class="pointer-events-none absolute h-12 w-12 rounded-full {backgrounds[
       type
     ]} opacity-0 blur-md"
     class:opacity-50={isActive}
   />
   <div
-    class="pointer-events-none absolute h-[46px] w-[46px] rotate-45 touch-none rounded-sm {backgrounds[
+    class="pointer-events-none absolute h-[46px] w-[46px] rotate-45 rounded-sm {backgrounds[
       type
     ]}"
   />
   <div
-    class="pointer-events-none absolute h-[52px] w-[52px] touch-none rounded-full {gradients[
+    class="pointer-events-none absolute h-[52px] w-[52px] rounded-full {gradients[
       type
     ]} outline outline-1 {outlines[type]}"
   />
   {#if type === 'artifact'}
     <img
-      class=" pointer-events-none z-10 h-[52px] w-[52px] touch-none rounded-full border-2 border-slate-200"
+      class=" pointer-events-none z-10 h-[52px] w-[52px] rounded-full border-2 border-slate-200"
       src="https://enka.network/ui/{url}_4.png"
       alt={url}
     />
   {:else if type === 'weapon'}
     <img
-      class=" pointer-events-none z-10 h-[52px] w-[52px] touch-none rounded-full border-2 border-slate-200"
+      class=" pointer-events-none z-10 h-[52px] w-[52px] rounded-full border-2 border-slate-200"
       src="https://enka.network/ui/{url}.png"
       alt={url}
     />
   {:else if type === 'resonance'}
     <img
-      class=" pointer-events-none z-10 h-[52px] w-[52px] touch-none rounded-full border-2 border-slate-200"
+      class=" pointer-events-none z-10 h-[52px] w-[52px] rounded-full border-2 border-slate-200"
       src="images/resonance/{url}.webp"
       alt={url}
     />
   {:else}
     <img
-      class=" pointer-events-none z-10 h-[52px] w-[52px] touch-none rounded-full border-2 border-slate-200"
+      class=" pointer-events-none z-10 h-[52px] w-[52px] rounded-full border-2 border-slate-200"
       src="https://enka.network/ui/{url}.png"
       alt={url}
     />
