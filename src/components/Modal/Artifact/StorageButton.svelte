66a8e11510b756989795c3f6a498be295444657c
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-11-29 18:51:18 -0400

    🌟🛠Refactor: StorageButton, btn now above data ✅

--- src/components/Modal/Artifact/StorageButton.svelte
+++ src/components/Modal/Artifact/StorageButton.svelte
@@ -49,97 +49,98 @@
   }
 </script>
 
-<div>
+<div
+  class="relative flex w-full items-center gap-2 rounded-xl bg-slate-700/80 pr-2 text-sm transition-colors"
+>
+  <div>
+    <Thumbnail
+      img={getImageUrl(type, storedArtifact.url, storedArtifact.selected)}
+      alt={storedArtifact.selected}
+      classes="w-12 xs-300:w-16 xs-375:w-20 xs-375:p-1"
+      hasBG={false}
+    />
+  </div>
+  <div class="flex w-full flex-col gap-2 py-3">
+    <!-- title -->
+    <div class="flex items-center justify-between">
+      <h3 class="truncate">
+        <span class="mr-1 font-bold text-slate-400">+{storedArtifact.lvl}</span
+        >{storedArtifact.fullName ?? 'Unidentifed'}
+      </h3>
+
+      <span
+        class:text-amber-500={storedArtifact.isFiveStar}
+        class:text-violet-500={!storedArtifact.isFiveStar}><Star class="w-4" /></span
+      >
+    </div>
+    <!-- main stat -->
+    <div class="flex items-center justify-between rounded bg-slate-800 py-1 pl-1.5 pr-2">
+      <div class="flex items-center gap-0.5">
+        <StatImage stat={storedArtifact.mainStat.stat} sm />
+        <h3 class="hidden font-bold uppercase text-slate-300 xs:block">
+          {LongStatLabels[storedArtifact.mainStat.stat]}
+        </h3>
+        <h3 class="font-bold uppercase text-slate-300 xs:hidden">
+          {StatLabels[storedArtifact.mainStat.stat]}
+        </h3>
+      </div>
+      <span class="font-bold text-slate-300">
+        +{artifactStatFormatter(
+          storedArtifact.mainStat.stat,
+          storedArtifact.mainStat.value
+        )}
+      </span>
+    </div>
+    <!-- substats -->
+    <div class="grid grid-cols-2 gap-1">
+      {#each storedArtifact.substats as substat}
+        {#if substat.stat}
+          <div class="flex items-center justify-start">
+            <StatImage stat={substat.stat} sm />
+            <span class="ml-0.5 hidden truncate text-slate-300 xs:block"
+              >{StatLabels[substat.stat]}</span
+            >
+            <span class="ml-2 font-bold text-[#DFD2B9] xs:ml-0">
+              +{artifactStatFormatter(substat.stat, substat.value)}
+            </span>
+          </div>
+        {:else}
+          <div
+            class="h-full w-full rounded border border-dashed border-slate-500 bg-slate-600"
+          />
+        {/if}
+      {/each}
+    </div>
+  </div>
   <button
     on:longpress={handleLongPress}
     use:longpress={300}
     disabled={deletable}
     on:click={() => handleArtifactClick(storedArtifact)}
-    class="flex w-full items-center gap-2 rounded-xl border bg-slate-700/80 pr-2 text-sm transition-colors"
+    class="absolute z-[5] h-full w-full rounded-xl border transition-colors"
     class:border-slate-600={!deletable}
     class:hover:border-slate-500={!deletable}
-    class:active:bg-slate-600={!deletable}
+    class:active:border-slate-300={!deletable}
     class:border-red-500={deletable}
     class:hover:border-red-500={deletable}
   >
-    <div>
-      <Thumbnail
-        img={getImageUrl(type, storedArtifact.url, storedArtifact.selected)}
-        alt={storedArtifact.selected}
-        classes="w-12 xs-300:w-16 xs-375:w-20 xs-375:p-1"
-        hasBG={false}
-      />
-    </div>
-    <div class="flex w-full flex-col gap-2 py-3">
-      <!-- title -->
-      <div class="flex items-center justify-between">
-        <h3 class="truncate">
-          <span class="mr-1 font-bold text-slate-400">+{storedArtifact.lvl}</span
-          >{storedArtifact.fullName ?? 'Unidentifed'}
-        </h3>
-
-        <span
-          class:text-amber-500={storedArtifact.isFiveStar}
-          class:text-violet-500={!storedArtifact.isFiveStar}><Star class="w-4" /></span
-        >
-      </div>
-      <!-- main stat -->
-      <div
-        class="flex items-center justify-between rounded bg-slate-800 py-1 pl-1.5 pr-2"
-      >
-        <div class="flex items-center gap-0.5">
-          <StatImage stat={storedArtifact.mainStat.stat} />
-          <h3 class="hidden font-bold uppercase text-slate-300 xs:block">
-            {LongStatLabels[storedArtifact.mainStat.stat]}
-          </h3>
-          <h3 class="font-bold uppercase text-slate-300 xs:hidden">
-            {StatLabels[storedArtifact.mainStat.stat]}
-          </h3>
-        </div>
-        <span class="font-bold text-slate-300">
-          +{artifactStatFormatter(
-            storedArtifact.mainStat.stat,
-            storedArtifact.mainStat.value
-          )}
-        </span>
-      </div>
-      <!-- substats -->
-      <div class="grid grid-cols-2 gap-1">
-        {#each storedArtifact.substats as substat}
-          {#if substat.stat}
-            <div class="flex items-center justify-start">
-              <StatImage stat={substat.stat} />
-              <span class="ml-0.5 hidden truncate text-slate-300 xs:block"
-                >{StatLabels[substat.stat]}</span
-              >
-              <span class="ml-2 font-bold text-[#DFD2B9] xs:ml-0">
-                +{artifactStatFormatter(substat.stat, substat.value)}
-              </span>
-            </div>
-          {:else}
-            <div
-              class="h-full w-full rounded border border-dashed border-slate-500 bg-slate-600"
-            />
-          {/if}
-        {/each}
-      </div>
-    </div>
+    <span class="hidden">Artifact Button</span>
   </button>
-  {#if deletable}
-    <div class="mt-2 flex justify-between gap-2 text-slate-200">
-      <button
-        class="w-full rounded-md border border-slate-600 bg-slate-700 p-2 transition-colors hover:border-slate-500 active:bg-slate-600"
-        on:click={() => (deletable = false)}
-      >
-        Cancel
-      </button>
-      <button
-        class="flex w-full items-center justify-center gap-1.5 rounded-md border border-red-700 bg-red-600 p-2 text-slate-100 transition-colors hover:border-red-500 active:bg-red-500"
-        on:click={() => handleDelete(storedArtifact.storageID)}
-      >
-        <Delete class="h-4 fill-slate-200" />
-        <span>Delete</span>
-      </button>
-    </div>
-  {/if}
 </div>
+{#if deletable}
+  <div class="flex justify-between gap-2 text-slate-200">
+    <button
+      class="w-full rounded-md border border-slate-600 bg-slate-700 p-2 transition-colors hover:border-slate-500 active:bg-slate-600"
+      on:click={() => (deletable = false)}
+    >
+      Cancel
+    </button>
+    <button
+      class="flex w-full items-center justify-center gap-1.5 rounded-md border border-red-700 bg-red-600 p-2 text-slate-100 transition-colors hover:border-red-500 active:bg-red-500"
+      on:click={() => handleDelete(storedArtifact.storageID)}
+    >
+      <Delete class="h-4 fill-slate-200" />
+      <span>Delete</span>
+    </button>
+  </div>
+{/if}
