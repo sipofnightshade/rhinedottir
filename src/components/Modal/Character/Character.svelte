759c2cdc2fc41c82a0de2f736c381407e0f2360d
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-12-01 16:12:24 -0400

    🌟🔥NEW: Character tabs + loadout empty state ✅

--- src/components/Modal/Character/Character.svelte
+++ src/components/Modal/Character/Character.svelte
@@ -1,151 +1,45 @@
 <script lang="ts">
   // components
-  import Thumbnail from '../../Thumbnail/Thumbnail.svelte';
-  import LevelGroup from '../_LevelGroup.svelte';
-  import StarGroup from '../../Stars/StarGroup.svelte';
-  import Filters from '../../Filters/Filters.svelte';
-  import Picker from '../../Picker/Picker.svelte';
+  import CharacterBuilder from './CharacterBuilder.svelte';
+  import CharacterLoadouts from './CharacterLoadouts.svelte';
 
-  // stores
-  import { character } from '$lib/stores/characterStore';
-  import { weapon } from '$lib/stores/weaponStore';
-  import { infusion } from '$lib/stores/infusionStore';
-  // data
-  import { DefaultWeapons } from '$lib/data/DefaultWeapons';
-  import { characterData } from '$lib/data/characters/index';
-  import { labels } from '$lib/data/Levels';
+  const items: any = [
+    { label: 'Builder', value: 1, component: CharacterBuilder },
+    { label: 'Loadouts', value: 2, component: CharacterLoadouts }
+  ];
 
-  // types
-  import type { SelectedWeapon } from '$lib/types/global';
-  import type { SelectedCharacter } from '$lib/types/global';
-  import { party } from '$lib/stores/partyStore';
+  let activeTabValue = 1;
 
-  // filter data
-  const filters = ['anemo', 'cryo', 'dendro', 'electro', 'geo', 'hydro', 'pyro'];
-
-  // state
-  let filter = '';
-  let filteredData = characterData;
-
-  let currentCharacter: SelectedCharacter;
-  let previousCharacter: SelectedCharacter;
-
-  // methods
-  function handleIncrement(event: any) {
-    character.increment(event.detail.groupID);
-  }
-
-  function handleDecrement(event: any) {
-    character.decrement(event.detail.groupID);
-  }
-
-  function handleCharacterSelect(event: any) {
-    character.setChar(event.detail.selected);
-    currentCharacter = $character.selected;
-
-    if ($character.selected.weapon !== $weapon.selected.type) {
-      weapon.setWeapon(DefaultWeapons[$character.selected.weapon] as SelectedWeapon);
-    }
-
-    // Reset infusion if the active character changes
-    if (currentCharacter !== previousCharacter) {
-      infusion.reset();
-    }
-
-    // Update the previousCharacter with the currentCharacter for the next comparison
-    previousCharacter = currentCharacter;
-  }
-
-  function removeDuplicateCharacter(selected: string) {
-    if (selected === $party.one?.character.selected.id) {
-      party.removePartyMember('one');
-    }
-
-    if (selected === $party.two?.character.selected.id) {
-      party.removePartyMember('two');
-    }
-
-    if (selected === $party.three?.character.selected.id) {
-      party.removePartyMember('three');
-    }
-  }
-
-  function handleFilters(event: any) {
-    if (event.detail.selected === filter) {
-      filter = '';
-    } else {
-      filter = event.detail.selected;
-    }
-  }
-
-  // reactive expressions
-  $: filteredData = filter
-    ? characterData.filter((item) => item.vision === filter)
-    : characterData;
-
-  $: removeDuplicateCharacter($character.selected.id);
-
-  $: charNormalLvl = $character.atk + $character.lvlBonus.atk + 1;
-  $: charSkillLvl = $character.skill + $character.lvlBonus.skill + 1;
-  $: charBurstLvl = $character.burst + $character.lvlBonus.burst + 1;
+  const handleClick = (tabValue: number) => () => (activeTabValue = tabValue);
 </script>
 
-<div class="flex h-full flex-col gap-4 overflow-hidden">
-  <div class="grid grid-cols-3 gap-x-2 gap-y-3">
-    <Thumbnail
-      img="https://enka.network/ui/{$character.selected.url}.png"
-      vision={$character.selected.vision}
-      alt={$character.selected.fullName}
-      rating={$character.selected.rating}
-    />
-    <div class="col-span-2 flex flex-col justify-end">
-      <div>
-        <h3 class="text-lg">{$character.selected.fullName}</h3>
-        <StarGroup rating={$character.selected.rating} />
+<!-- Artifact TABS -->
+<div class="flex h-full flex-col gap-2 overflow-hidden xs:gap-4">
+  <ul
+    class="grid list-none grid-cols-2 justify-between rounded-full border border-slate-600 bg-slate-700 p-0"
+  >
+    {#each items as item}
+      <li class="flex items-center justify-center rounded-full">
+        <button
+          class="h-full w-full rounded-full py-2.5"
+          on:click={handleClick(item.value)}
+          class:bg-slate-800={activeTabValue === item.value}
+          class:text-slate-100={activeTabValue === item.value}>{item.label}</button
+        >
+      </li>
+    {/each}
+  </ul>
+  <section class="flex h-full flex-col overflow-hidden">
+    {#each items as item (item.value)}
+      <!-- {#if activeTabValue === item.value} -->
+      <div
+        class="flex h-full w-full flex-col overflow-hidden {activeTabValue === item.value
+          ? ''
+          : 'hidden'}"
+      >
+        <svelte:component this={item.component} />
       </div>
-      <div class="grid grid-cols-2 gap-x-2">
-        <LevelGroup
-          label="Lvl"
-          value={$character.lvl}
-          id="lvl"
-          on:increment={handleIncrement}
-          on:decrement={handleDecrement}
-        />
-        <LevelGroup
-          label="Const."
-          value={$character.constellation}
-          id="constellation"
-          on:increment={handleIncrement}
-          on:decrement={handleDecrement}
-        />
-      </div>
-    </div>
-    <LevelGroup
-      label="Normal"
-      value={charNormalLvl}
-      id="atk"
-      plusThree={$character.lvlBonus.atk > 0}
-      on:increment={handleIncrement}
-      on:decrement={handleDecrement}
-    />
-    <LevelGroup
-      label="Skill"
-      value={charSkillLvl}
-      id="skill"
-      plusThree={$character.lvlBonus.skill > 0}
-      on:increment={handleIncrement}
-      on:decrement={handleDecrement}
-    />
-    <LevelGroup
-      label="Burst"
-      value={charBurstLvl}
-      id="burst"
-      plusThree={$character.lvlBonus.burst > 0}
-      on:increment={handleIncrement}
-      on:decrement={handleDecrement}
-    />
-  </div>
-
-  <Filters selected={filter} {filters} on:filter={handleFilters} />
-  <Picker on:selected={handleCharacterSelect} data={filteredData} type="character" />
+      <!-- {/if} -->
+    {/each}
+  </section>
 </div>
