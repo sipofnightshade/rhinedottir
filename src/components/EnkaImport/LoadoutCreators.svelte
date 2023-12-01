37cdaa6beb4c203f281b44e775e994e5e511ba02
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-12-01 16:13:07 -0400

    🛠Update: Hide LoadoutCreator empty circles ✅

--- src/components/EnkaImport/LoadoutCreators.svelte
+++ src/components/EnkaImport/LoadoutCreators.svelte
@@ -4,20 +4,22 @@
   export let playerBuilds: any[];
 </script>
 
-<div class="mt-3">
-  <div
-    class="horizontalScrollbar mt-2 flex gap-2 overflow-auto py-1 md:justify-between md:py-0"
-  >
-    {#each Array(8) as _, index (index)}
-      {#if playerBuilds[index]}
-        <ImportButton build={playerBuilds[index]} />
-      {:else}
-        <div
-          class="flex aspect-square h-12 items-center justify-center rounded-full border border-dashed border-slate-400 md:h-11"
-        >
-          <TeamIcon class="w-1/2" />
-        </div>
-      {/if}
-    {/each}
+{#if playerBuilds.length > 0}
+  <div class="mt-3 transition-transform">
+    <div
+      class="horizontalScrollbar mt-2 flex gap-2 overflow-auto py-1 md:justify-between md:py-0"
+    >
+      {#each Array(8) as _, index (index)}
+        {#if playerBuilds[index]}
+          <ImportButton build={playerBuilds[index]} />
+        {:else}
+          <div
+            class="flex aspect-square h-12 items-center justify-center rounded-full border border-dashed border-slate-400 md:h-11"
+          >
+            <TeamIcon class="w-1/2" />
+          </div>
+        {/if}
+      {/each}
+    </div>
   </div>
-</div>
+{/if}
