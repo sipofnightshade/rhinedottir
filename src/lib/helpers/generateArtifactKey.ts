56998f4b9c60751007078a4ce61e5d7b1508f50c
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-12-02 19:06:15 -0400

    🌟New: Function to generate artifact ID's✅

--- src/lib/helpers/generateArtifactKey.ts
+++ src/lib/helpers/generateArtifactKey.ts
@@ -1,10 +1,14 @@
-import type { SavedArtifactItem } from '$lib/types/loadout';
+import type { ArtifactNames, ArtifactStats } from '$lib/types/artifacts';
 
-export function generateArtifactKey(artifact: SavedArtifactItem): string {
-  const { selected, lvl, mainStat, substats } = artifact;
+type STAT = { stat: ArtifactStats; value: number };
 
+export function generateArtifactKey(
+  selected: ArtifactNames,
+  mainStat: STAT,
+  substats: STAT[]
+): string {
   const main = `${mainStat.stat}${mainStat.value}`;
   const subs = substats.map((substat) => `${substat.stat}${substat.value}`).join('-');
 
-  return `${selected}-${lvl}-${main}-${subs}`;
+  return `${selected}-${main}-${subs}`;
 }
