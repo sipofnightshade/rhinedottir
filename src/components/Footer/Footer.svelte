20dff9f58b58aaddccde9a2debdaeecaf0de0f88
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-12-10 09:19:18 -0400

    🛠Fix: Rm Privacy redirect

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -6,8 +6,7 @@
   <div class="flex items-center gap-2 text-slate-400">
     <a
       class="text-sm uppercase transition-colors hover:text-slate-300 active:text-slate-400"
-      href="/privacy-policy"
-      target="_blank">Privacy</a
+      href="/privacy-policy">Privacy</a
     >
     <div class="h-3/5 w-0.5 bg-slate-400" />
     <p class="text-sm uppercase">V.1</p>
