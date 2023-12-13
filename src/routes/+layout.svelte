9e00d3605502c24d166d2e800565704601561611
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2023-12-13 12:54:08 -0400

    🛠Update: use adsense meta tag instead of script

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -35,11 +35,7 @@
   />
   <meta property="twitter:image" content="share-screen-square.png" />
 
-  <script
-    async
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3936138317575261"
-    crossorigin="anonymous"
-  ></script>
+  <meta name="google-adsense-account" content="ca-pub-3936138317575261" />
 </svelte:head>
 
 <!-- <Analytics /> -->
